88a6fabee45824f21d23330eb09a935841e71012
Mateusz Loskot <mateusz@loskot.net>
2010-02-23 00:31:24 +0000

    Geometry: testing doxygen and quickbook integration with /// style comments and formatting of varios elements of synopsis.

--- include/boost/geometry/core/access.hpp
+++ include/boost/geometry/core/access.hpp
@@ -9,30 +9,29 @@
 #ifndef BOOST_GEOMETRY_CORE_ACCESS_HPP
 #define BOOST_GEOMETRY_CORE_ACCESS_HPP
 
-
-
-/*!
-\defgroup access access: get/set coordinate values, make objects, clear geometries, append point(s)
-\details There are many ways to edit geometries. It is possible to:
-- use the geometries themselves, so access point.x(). This is not done inside the library because it is agnostic
- to geometry type. However, library users can use this as it is intuitive.
-- use the standard library, so use .push_back(point) or use inserters. This is also avoided inside the library.
-However, library users can use it if they are used to the standard library
-- use the functionality provided in this geometry library. These are the functions in this module.
-
-The library provides the following functions to edit geometries:
-- set to set one coordinate value
-- assign to set two or more coordinate values
-- make to construct and return geometries with specified coordinates.
-- append to append one or more points to a geometry
-- clear to remove all points from a geometry
-
-For getting coordinates it is similar:
-- get to get a coordinate value
-- or use the standard library
-- or use the geometries themselves
-
-*/
+/// @defgroup access access: get/set coordinate values, make objects, clear geometries, append point(s)
+/// @details There are many ways to edit geometries. It is possible to:
+///
+/// @li use the geometries themselves, so access point.x(). This is not done
+/// inside the library because it is agnostic to geometry type. However,
+/// library users can use this as it is intuitive.
+/// @li use the standard library, so use .push_back(point) or use inserters.
+/// This is also avoided inside the library. However, library users can use
+/// it if they are used to the standard library
+/// @li use the functionality provided in this geometry library. These are
+/// the functions in this module.
+/// 
+/// The library provides the following functions to edit geometries:
+/// @li set to set one coordinate value
+/// @li assign to set two or more coordinate values
+/// @li make to construct and return geometries with specified coordinates.
+/// @li append to append one or more points to a geometry
+/// @li clear to remove all points from a geometry
+/// 
+/// For getting coordinates it is similar:
+/// @li get to get a coordinate value
+/// @li or use the standard library
+/// @li or use the geometries themselves
 
 #include <cstddef>
 
@@ -44,26 +43,25 @@ For getting coordinates it is similar:
 #include <boost/geometry/core/tag.hpp>
 
 
-namespace boost { namespace geometry
-{
+namespace boost { namespace geometry {
 
+/// Index of minimum corner of the box.
 const int min_corner = 0;
+
+/// Index of maximum corner of the box.
 const int max_corner = 1;
 
-namespace traits
-{
+namespace traits {
 
-/*!
-\brief Traits class which gives access (get,set) to points
-\ingroup traits
-\par Geometries:
-    - point
-\par Specializations should provide, per Dimension
-    - static inline T get(const G&)
-    - static inline void set(G&, T const&)
-\tparam Geometry geometry-type
-\tparam Dimension dimension to access
-*/
+/// @brief Traits class which gives access (get,set) to points.
+/// @ingroup traits
+/// @par Geometries:
+///     @li point
+/// @par Specializations should provide, per Dimension
+///     @li static inline T get(const G&)
+///     @li static inline void set(G&, T const&)
+/// @tparam Geometry geometry-type
+/// @tparam Dimension dimension to access
 template <typename Geometry, std::size_t Dimension>
 struct access {};
 
