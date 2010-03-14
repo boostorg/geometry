c82ae0ec8852e57bfcf8a7f170f4b686c2b0b2e0
Barend Gehrels <barend@xs4all.nl>
2010-03-14 14:30:06 +0000

    Updated dissolve ("dissolve" renamed to "dissolve_inserter", added free function "dissolve") Added dissolve for multipolygons

--- include/boost/geometry/multi/algorithms/dissolve.hpp
+++ include/boost/geometry/multi/algorithms/dissolve.hpp
@@ -17,23 +17,82 @@ namespace boost { namespace geometry
 {
 
 
+#ifndef DOXYGEN_NO_DETAIL
+namespace detail { namespace dissolve
+{
+
+template <typename Multi, typename MultiOut>
+struct dissolve_multi
+{
+	template <typename OutputIterator>
+    static inline OutputIterator apply(Multi const& multi, OutputIterator out)
+    {
+		typedef typename boost::range_value<Multi>::type polygon_type;
+		typedef typename boost::range_value<MultiOut>::type output_polygon_type;
+		typedef typename boost::range_iterator<Multi const>::type iterator_type;
+		
+		// Step 1: dissolve all polygons in the multi-polygon, independantly
+		MultiOut step1;
+        for (iterator_type it = boost::begin(multi);
+            it != boost::end(multi);
+            ++it)
+        {
+			dissolve_ring_or_polygon
+				<
+					polygon_type,
+					output_polygon_type
+				>::apply(*it, std::back_inserter(step1));
+		}
+
+		// Step 2: remove mutual overlap
+		// TODO: solve quadratic behaviour by alternating division in x/y axis
+		// per division there are 3 cases: cases above the line, cases below the line, cases crossing the line
+		// recursively handle those 3 cases and union them.
+		MultiOut step2;
+        for (iterator_type it = boost::begin(step1);
+            it != boost::end(step1);
+            ++it)
+        {
+			if (step2.empty())
+			{
+				step2.push_back(*it);
+			}
+			else
+			{
+				MultiOut unioned;
+				for (iterator_type it2 = boost::begin(step2);
+					it2 != boost::end(step2);
+					++it2)
+				{
+					geometry::union_inserter
+						<
+							output_polygon_type
+						>(*it2, *it, std::back_inserter(unioned));
+				}
+				step2.swap(unioned);
+			}
+		}
+
+		// Step 3: output
+		*out++ = step2;
+		return out;
+	}
+};
+
+
+}} // namespace detail::dissolve
+#endif
+
+
+
 #ifndef DOXYGEN_NO_DISPATCH
 namespace dispatch
 {
 
 
-template
-<
-    typename Multi,
-    typename OutputIterator
->
-struct dissolve
-    <
-        multi_polygon_tag,
-        Multi,
-        OutputIterator
-    >
-    : detail::dissolve::dissolve_region<Multi, OutputIterator>
+template<typename Multi, typename MultiOut>
+struct dissolve<multi_polygon_tag, multi_polygon_tag, Multi, MultiOut>
+    : detail::dissolve::dissolve_multi<Multi, MultiOut>
 {};
 
 
