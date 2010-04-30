bceab2b0f2c5567f4e439df5f53171b3643446d7
Barend Gehrels <barend@xs4all.nl>
2010-04-30 16:22:23 +0000

    Update for gcc
    [SVN r61709]

--- include/boost/geometry/util/reversible_view.hpp
+++ include/boost/geometry/util/reversible_view.hpp
@@ -49,7 +49,7 @@ template <typename Range>
 struct reversible_view<Range, iterate_forward> : detail::reversible_base_view<Range>
 {
     reversible_view(Range& r)
-        : reversible_base_view(r)
+        : reversible_base_view<Range>(r)
     {}
 
     typedef typename boost::range_iterator<Range const>::type const_iterator;
@@ -67,7 +67,7 @@ template <typename Range>
 struct reversible_view<Range, iterate_reverse> : detail::reversible_base_view<Range>
 {
     reversible_view(Range& r)
-        : reversible_base_view(r)
+        : reversible_base_view<Range>(r)
     {}
 
     typedef typename boost::range_reverse_iterator<Range const>::type const_iterator;
@@ -79,7 +79,6 @@ struct reversible_view<Range, iterate_reverse> : detail::reversible_base_view<Ra
     iterator begin() { return boost::rbegin(m_range); }
     iterator end() { return boost::rend(m_range); }
 };
-
 
 
 }} // namespace boost::geometry
