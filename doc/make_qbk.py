914671f0c5c2505a822f8f6184ce7a2cb1f3916a
Barend Gehrels <barend@xs4all.nl>
2011-01-01 16:22:40 +0000

    Doc update, enhanced converter, added many groups, fixed many endpoints

--- doc/make_qbk.py
+++ doc/make_qbk.py
@@ -22,31 +22,47 @@ def call_doxygen():
 	os.chdir("..")
 
 def group_to_quickbook(section):
-	os.system(cmd % ("group__" + section, section))
+	os.system(cmd % ("group__" + section.replace("_", "__"), section))
 
-def xml_to_quickbook(xml, section):
-	os.system(cmd % (xml, section))
+def model_to_quickbook(classname, section):
+	os.system(cmd % ("classboost_1_1geometry_1_1model_1_1" + classname, section))
 
 
 call_doxygen()
 
-algorithms = ["area", "buffer", "centroid", "combine", "convert" 
+algorithms = ["append", "assign", "make", "clear"
+	, "area", "buffer", "centroid", "combine", "convert" 
 	, "convex_hull", "difference", "disjoint", "dissolve", "distance" 
 	, "envelope", "equals", "for_each", "intersection", "intersects" 
 	, "length", "num_geometries", "num_interior_rings", "num_points" 
 	, "overlaps", "perimeter", "reverse", "simplify", "sym_difference" 
 	, "transform", "union", "unique", "within"]
+access_functions = ["get", "set", "exterior_ring", "interior_rings"
+	, "num_points", "num_interior_rings", "num_geometries"]
 
 for a in algorithms:
 	group_to_quickbook(a)
+	
+for a in access_functions:
+	group_to_quickbook(a)
 
+group_to_quickbook("arithmetic")
 group_to_quickbook("register")
 
-xml_to_quickbook("classboost_1_1geometry_1_1model_1_1point", "point")
-xml_to_quickbook("classboost_1_1geometry_1_1model_1_1d2_1_1point__xy", "point_xy")
-xml_to_quickbook("classboost_1_1geometry_1_1model_1_1linestring", "linestring")
-xml_to_quickbook("classboost_1_1geometry_1_1model_1_1box", "box")
+model_to_quickbook("point", "point")
+model_to_quickbook("d2_1_1point__xy", "point_xy")
+model_to_quickbook("linestring", "linestring")
+model_to_quickbook("box", "box")
+model_to_quickbook("polygon", "polygon")
+model_to_quickbook("segment", "segment")
+model_to_quickbook("multi__linestring", "multi_linestring")
+model_to_quickbook("multi__point", "multi_point")
+model_to_quickbook("multi__polygon", "multi_polygon")
+model_to_quickbook("linear__ring", "ring")
+model_to_quickbook("referring__segment", "referring_segment")
 
-xml_to_quickbook("structboost_1_1geometry_1_1closing__iterator", "closing_iterator")
+
+
+#model_to_quickbook("structboost_1_1geometry_1_1closing__iterator", "closing_iterator")
 
 os.system("bjam") 
