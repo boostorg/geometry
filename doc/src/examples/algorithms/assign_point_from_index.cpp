5de18e288105784b4471066973279640778a3fa1
Barend Gehrels <barend@xs4all.nl>
2011-02-21 22:26:05 +0000

    Doc update
    [SVN r69128]

---
+++ doc/src/examples/algorithms/assign_point_from_index.cpp
@@ -0,0 +1,51 @@
+// Boost.Geometry (aka GGL, Generic Geometry Library)
+//
+// Copyright Barend Gehrels 2011, Geodan, Amsterdam, the Netherlands
+// Use, modification and distribution is subject to the Boost Software License,
+// Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+// Quickbook Example
+
+//[assign_point_from_index
+//` Shows how to retrieve one point from a box (either lower-left or upper-right) or segment
+
+#include <iostream>
+
+#include <boost/geometry/geometry.hpp>
+#include <boost/geometry/geometries/geometries.hpp>
+
+using namespace boost::geometry;
+
+int main()
+{
+    typedef model::d2::point_xy<double> point;
+    typedef model::segment<point> segment;
+
+    segment s;
+    assign(s, 1, 1, 2, 2);
+
+    point first, second;
+    assign_point_from_index<0>(s, first);
+    assign_point_from_index<1>(s, second);
+    std::cout
+        << "segment: " << dsv(s) << std::endl
+        << "first: " << dsv(first) << std::endl
+        << "second: " << dsv(second) << std::endl;
+
+    return 0;
+}
+
+//]
+
+
+//[assign_point_from_index_output
+/*`
+Output:
+[pre
+segment: ((1, 1), (2, 2))
+first: (1, 1)
+second: (2, 2)
+]
+*/
+//]
