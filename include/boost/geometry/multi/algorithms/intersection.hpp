6ef5f12a0cb5daf4583c2cbf5b1ea4c5272fba34
Barend Gehrels <barend@xs4all.nl>
2011-03-05 23:27:06 +0000

    Fixed error in new assemble approach, now both recursive_boxes as intersection_pies are completely running (again) for all combinations of open,cw,ccw,multi,box,triangle

--- include/boost/geometry/multi/algorithms/intersection.hpp
+++ include/boost/geometry/multi/algorithms/intersection.hpp
@@ -9,7 +9,6 @@
 #define BOOST_GEOMETRY_MULTI_ALGORITHMS_INTERSECTION_HPP
 
 
-#include <boost/geometry/algorithms/intersection.hpp>
 #include <boost/geometry/multi/core/closure.hpp>
 #include <boost/geometry/multi/core/geometry_id.hpp>
 #include <boost/geometry/multi/core/is_areal.hpp>
@@ -23,6 +22,8 @@
 #include <boost/geometry/multi/algorithms/detail/overlay/select_rings.hpp>
 #include <boost/geometry/multi/algorithms/detail/sections/range_by_section.hpp>
 #include <boost/geometry/multi/algorithms/detail/sections/sectionalize.hpp>
+
+#include <boost/geometry/algorithms/intersection.hpp>
 
 
 namespace boost { namespace geometry
