9b5d633ce198bb6fbf474cf4d9338335ac97827b
Barend Gehrels <barend@xs4all.nl>
2011-03-11 14:36:50 +0000

    Added tool "implementation_status"
    [SVN r69847]

---
+++ doc/src/docutils/tools/implementation_status/implementation_status.hpp
@@ -0,0 +1,12 @@
+// implementation_status (developed in the context of Boost.Geometry documentation)
+//
+// Copyright Barend Gehrels 2010, 2011, Geodan, Amsterdam, the Netherlands
+// Use, modification and distribution is subject to the Boost Software License,
+// Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#include <boost/geometry/geometry.hpp>
+#include <boost/geometry/multi/multi.hpp>
+#include <boost/geometry/multi/geometries/multi_point.hpp>
+#include <boost/geometry/extensions/gis/io/wkt/wkt.hpp>
+
