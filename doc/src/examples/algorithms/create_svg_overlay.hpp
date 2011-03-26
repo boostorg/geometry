21bb9b38f13b109df42648e0a0e97ac238f56036
Barend Gehrels <barend@xs4all.nl>
2011-03-26 22:55:54 +0000

    Updated copyright messages in examples
    [SVN r70602]

--- doc/src/examples/algorithms/create_svg_overlay.hpp
+++ doc/src/examples/algorithms/create_svg_overlay.hpp
@@ -1,11 +1,13 @@
 // Boost.Geometry (aka GGL, Generic Geometry Library)
-//
-// Copyright Barend Gehrels 2011, Geodan, Amsterdam, the Netherlands
+
+// Copyright (c) 2011 Barend Gehrels, Amsterdam, the Netherlands.
+
 // Use, modification and distribution is subject to the Boost Software License,
 // Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
-//
+
 // Code to create SVG for examples
+
 #ifndef CREATE_SVG_OVERLAY_HPP
 #define CREATE_SVG_OVERLAY_HPP
 
