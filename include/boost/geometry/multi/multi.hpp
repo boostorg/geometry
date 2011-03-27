008c4efc58931a516ec7caef6521498589b4d3c7
Barend Gehrels <barend@xs4all.nl>
2011-03-27 18:54:13 +0000

    Removed obsolete file

--- include/boost/geometry/multi/multi.hpp
+++ include/boost/geometry/multi/multi.hpp
@@ -7,7 +7,6 @@
 // Parts of Boost.Geometry are redesigned from Geodan's Geographic Library
 // (geolib/GGL), copyright (c) 1995-2010 Geodan, Amsterdam, the Netherlands.
 
-// Copyright Mateusz Loskot 2009
 // Use, modification and distribution is subject to the Boost Software License,
 // Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
@@ -71,7 +70,6 @@
 
 #include <boost/geometry/multi/iterators/range_type.hpp>
 
-#include <boost/geometry/multi/strategies/centroid.hpp>
 #include <boost/geometry/multi/strategies/cartesian/centroid_average.hpp>
 
 #include <boost/geometry/multi/util/write_dsv.hpp>
