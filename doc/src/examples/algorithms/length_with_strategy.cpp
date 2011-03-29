547a5bd9db60116be271a3fc6c61e54c0ab5efa1
Barend Gehrels <barend@xs4all.nl>
2011-03-29 21:29:32 +0000

    Small change in comment location

--- doc/src/examples/algorithms/length_with_strategy.cpp
+++ doc/src/examples/algorithms/length_with_strategy.cpp
@@ -1,12 +1,11 @@
 // Boost.Geometry (aka GGL, Generic Geometry Library)
+// QuickBook Example
 
 // Copyright (c) 2011 Barend Gehrels, Amsterdam, the Netherlands.
 
 // Use, modification and distribution is subject to the Boost Software License,
 // Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
-
-// Quickbook Example
 
 //[length_with_strategy
 //`The following example shows the length measured over a sphere, expressed in kilometers. To do that the radius of the sphere must be specified in the constructor of the strategy.
