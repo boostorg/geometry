eefb1b60568b75b4ecfd51208dc83b388576d9ae
Barend Gehrels <barend@xs4all.nl>
2011-04-02 21:55:40 +0000

    Moved units to extension
    [SVN r70898]

--- include/boost/geometry/extensions/geometries/quantity_point.hpp
+++ include/boost/geometry/extensions/geometries/quantity_point.hpp
@@ -11,8 +11,8 @@
 // Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
-#ifndef BOOST_GEOMETRY_GEOMETRIES_QUANTITY_POINT_HPP
-#define BOOST_GEOMETRY_GEOMETRIES_QUANTITY_POINT_HPP
+#ifndef BOOST_GEOMETRY_EXTENSIONS_GEOMETRIES_QUANTITY_POINT_HPP
+#define BOOST_GEOMETRY_EXTENSIONS_GEOMETRIES_QUANTITY_POINT_HPP
 
 #include <cstddef>
 
@@ -131,4 +131,4 @@ struct access<model::quantity_point<Units, DimensionCount, CoordinateType, Coord
 
 }} // namespace boost::geometry
 
-#endif // BOOST_GEOMETRY_GEOMETRIES_QUANTITY_POINT_HPP
+#endif // BOOST_GEOMETRY_EXTENSIONS_GEOMETRIES_QUANTITY_POINT_HPP
