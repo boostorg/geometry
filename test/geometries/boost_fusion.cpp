3697a12fb1c9de62e9827c48531caf1ad70b5b1a
Bruno Lalande <bruno.lalande@gmail.com>
2011-04-25 08:32:51 +0000

    Removed adapted/fusion_<cs>.hpp files and added a macro to use instead.

--- test/geometries/boost_fusion.cpp
+++ test/geometries/boost_fusion.cpp
@@ -13,12 +13,12 @@
 #include <boost/fusion/include/adapt_struct_named.hpp>
 
 #include <boost/geometry/geometry.hpp>
-#include <boost/geometry/geometries/adapted/fusion.hpp>
-#include <boost/geometry/geometries/adapted/fusion_cartesian.hpp>
-#include<boost/geometry/geometries/adapted/c_array_cartesian.hpp>
+#include <boost/geometry/geometries/adapted/boost_fusion.hpp>
+#include <boost/geometry/geometries/adapted/c_array_cartesian.hpp>
 #include <boost/geometry/geometries/adapted/boost_tuple.hpp>
 #include <iostream>
 
+BOOST_GEOMETRY_REGISTER_BOOST_FUSION_CS(cs::cartesian);
 BOOST_GEOMETRY_REGISTER_BOOST_TUPLE_CS(cs::cartesian);
 
 
