0fe54a1a8d26cdbffdba9822659bfb34c6d81ebf
Barend Gehrels <barend@xs4all.nl>
2011-05-04 20:41:17 +0000

    Added test for box-in-box
    [SVN r71722]

--- test/algorithms/within.cpp
+++ test/algorithms/within.cpp
@@ -33,8 +33,13 @@ void test_all()
     test_geometry<P, bg::model::polygon<P> >("POINT(2 2)",
         "POLYGON((0 0,0 4,4 4,4 0,0 0),(1 1,3 1,3 3,1 3,1 1))", false);
 
+    typedef bg::model::box<P> box_type;
 
-    test_geometry<P, bg::model::box<P> >("POINT(1 1)", "BOX(0 0,2 2)", true);
+    test_geometry<P, box_type>("POINT(1 1)", "BOX(0 0,2 2)", true);
+
+    test_geometry<box_type, box_type>("BOX(1 1,2 2)", "BOX(0 0,3 3)", true);
+    test_geometry<box_type, box_type>("BOX(0 0,3 3)", "BOX(1 1,2 2)", false);
+
 
     // Mixed point types
     test_geometry
@@ -45,7 +50,7 @@ void test_all()
 
 
     // Real-life problem (solved now), point is in the middle, 409623 is also a coordinate
-    // on the border, it was wrong first.
+    // on the border, has been wrong in the past (2009)
     test_ring<P>("POINT(146383 409623)",
         "POLYGON((146351 410597,146521 410659,147906 410363,148088 410420"
         ",148175 410296,148281 409750,148215 409623,148154 409666,148154 409666"
