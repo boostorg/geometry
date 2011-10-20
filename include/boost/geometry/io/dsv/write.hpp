531f8c084aae4478255cba9bb2eef8a24632c2af
Mateusz Loskot <mateusz@loskot.net>
2011-10-20 22:34:10 +0000

    [geometry] Updated include guards to reflect current file path

--- include/boost/geometry/io/dsv/write.hpp
+++ include/boost/geometry/io/dsv/write.hpp
@@ -11,8 +11,8 @@
 // Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
-#ifndef BOOST_GEOMETRY_IO_WRITE_DSV_HPP
-#define BOOST_GEOMETRY_IO_WRITE_DSV_HPP
+#ifndef BOOST_GEOMETRY_IO_DSV_WRITE_HPP
+#define BOOST_GEOMETRY_IO_DSV_WRITE_HPP
 
 
 #include <cstddef>
@@ -393,4 +393,4 @@ inline detail::dsv::dsv_manipulator<Geometry> dsv(Geometry const& geometry
 }} // namespace boost::geometry
 
 
-#endif // BOOST_GEOMETRY_IO_WRITE_DSV_HPP
+#endif // BOOST_GEOMETRY_IO_DSV_WRITE_HPP
