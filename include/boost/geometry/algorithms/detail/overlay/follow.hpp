ae52ce120acb33682b68c84f52a16242f050c1ac
Barend Gehrels <barend@xs4all.nl>
2011-11-19 16:37:55 +0000

    Linestring/polygon overlay, second phase (including touching intersection points)
    [SVN r75555]

--- include/boost/geometry/algorithms/detail/overlay/follow.hpp
+++ include/boost/geometry/algorithms/detail/overlay/follow.hpp
@@ -31,24 +31,9 @@ namespace detail { namespace overlay
 {
 
 
-template<typename Turn>
-struct sort_on_segment
-{
-    inline bool operator()(Turn const& left, Turn const& right) const
-    {
-        segment_identifier const& sl = left.operations[0].seg_id;
-        segment_identifier const& sr = right.operations[0].seg_id;
-
-        return sl == sr
-            ? left.operations[0].enriched.distance < right.operations[0].enriched.distance
-            : sl < sr;
-
-    }
-};
-
 
 /*!
-\brief Follows a linestring from intersection to intersection, outputting which
+\brief Follows a linestring from intersection point to intersection point, outputting which
     is inside, or outside, a ring or polygon
 \ingroup overlay
  */
@@ -56,49 +41,132 @@ template
 <
     typename LineStringOut,
     typename LineString,
-    typename Turns,
-    typename OutputIterator
+    typename Polygon
 >
-static inline OutputIterator follow(LineString const& geometry1,
-            detail::overlay::operation_type operation,
-            Turns& turns, OutputIterator out)
+class follow
 {
-    typedef typename boost::range_iterator<Turns>::type turn_iterator;
-    typedef typename boost::range_value<Turns>::type turn_type;
-    typedef typename boost::range_iterator
-        <
-            typename turn_type::container_type
-        >::type turn_operation_iterator_type;
 
-
-    // Sort intersection points on segments-along-linestring, and distance
-    // (like in enrich is done for poly/poly)
-    std::sort(boost::begin(turns), boost::end(turns), sort_on_segment<turn_type>());
-
-    LineStringOut current_piece;
-    geometry::segment_identifier current_segment_id(0, -1, -1, -1);
-
-    // Iterate through all intersection points (they are ordered along the line)
-    bool entered = false;
-    for (turn_iterator it = boost::begin(turns); it != boost::end(turns); ++it)
+    template<typename Turn>
+    struct sort_on_segment
     {
-        // Skip discarded/blocked ones TODO
-        ////if (! (it->is_discarded() || it->blocked()))
+        inline bool operator()(Turn const& left, Turn const& right) const
+        {
+            segment_identifier const& sl = left.operations[0].seg_id;
+            segment_identifier const& sr = right.operations[0].seg_id;
+
+            return sl == sr
+                ? left.operations[0].enriched.distance < right.operations[0].enriched.distance
+                : sl < sr;
+
+        }
+    };
+
+    template <typename Turn, typename Operation>
+    static inline bool is_entering(Turn const& turn, Operation const& op)
+    {
+        return op.operation == operation_intersection;
+    }
+
+    template <typename Turn, typename Operation>
+    static inline bool is_leaving(Turn const& turn, Operation const& op, 
+                    bool entered, bool first, 
+                    LineString const& linestring, Polygon const& polygon)
+    {
+        if (op.operation == operation_union)
+        {
+            if (turn.method == method_crosses)
+            {
+                return true;
+            }
+
+            if (entered)
+            {
+                return true;
+            }
+            if (first)
+            {
+                // Check if first point of line is inside polygon
+                return geometry::within(linestring[0], polygon);
+            }
+        }
+        return false;
+    }
+
+
+    template <typename Turn, typename Operation>
+    static inline bool is_staying_inside(Turn const& turn, Operation const& op, 
+                    bool entered, bool first, 
+                    LineString const& linestring, Polygon const& polygon)
+    {
+        if (turn.method == method_crosses)
+        {
+            return false;
+        }
+
+        if (op.operation == operation_intersection)
+        {
+            if (entered)
+            {
+                return true;
+            }
+            if (first)
+            {
+                // Check if first point of line is inside polygon
+                return geometry::within(linestring[0], polygon);
+            }
+        }
+        return false;
+    }
+
+
+public :
+    template<typename Turns, typename OutputIterator>
+    static inline OutputIterator apply(LineString const& linestring, Polygon const& polygon,
+                detail::overlay::operation_type operation,
+                Turns& turns, OutputIterator out)
+    {
+        typedef typename boost::range_iterator<Turns>::type turn_iterator;
+        typedef typename boost::range_value<Turns>::type turn_type;
+        typedef typename boost::range_iterator
+            <
+                typename turn_type::container_type
+            >::type turn_operation_iterator_type;
+
+
+        // Sort intersection points on segments-along-linestring, and distance
+        // (like in enrich is done for poly/poly)
+        std::sort(boost::begin(turns), boost::end(turns), sort_on_segment<turn_type>());
+
+        LineStringOut current_piece;
+        geometry::segment_identifier current_segment_id(0, -1, -1, -1);
+
+        // Iterate through all intersection points (they are ordered along the line)
+        bool entered = false;
+        bool first = true;
+        for (turn_iterator it = boost::begin(turns); it != boost::end(turns); ++it)
         {
             turn_operation_iterator_type iit = boost::begin(it->operations);
 
-            if (iit->operation == operation_intersection)
+            if (is_staying_inside(*it, *iit, entered, first, linestring, polygon))
             {
-                // Entering
+                debug_traverse(*it, *iit, "-> Staying inside");
+
+                entered = true;
+            }
+            else if (is_entering(*it, *iit))
+            {
+                debug_traverse(*it, *iit, "-> Entering");
+
                 entered = true;
                 detail::overlay::append_no_duplicates(current_piece, it->point);
                 current_segment_id = iit->seg_id;
             }
-            else if (iit->operation == operation_union)
+            else if (is_leaving(*it, *iit, entered, first, linestring, polygon))
             {
-                // Leaving
+                debug_traverse(*it, *iit, "-> Leaving");
+
                 entered = false;
-                geometry::copy_segments<false>(geometry1, current_segment_id,
+                geometry::copy_segments<false>(linestring, current_segment_id,
                         iit->seg_id.segment_index,
                         current_piece);
                 detail::overlay::append_no_duplicates(current_piece, it->point);
@@ -109,23 +177,24 @@ static inline OutputIterator follow(LineString const& geometry1,
                     current_piece.clear();
                 }
             }
+            first = false;
         }
-    }
 
-    if (entered)
-    {
-        geometry::copy_segments<false>(geometry1, current_segment_id, 
-                boost::size(geometry1) - 1,
-                current_piece);
-    }
+        if (entered)
+        {
+            geometry::copy_segments<false>(linestring, current_segment_id, 
+                    boost::size(linestring) - 1,
+                    current_piece);
+        }
 
-    // Add the last one, if applicable
-    if (! current_piece.empty())
-    {
-        *out++ = current_piece;
+        // Add the last one, if applicable
+        if (! current_piece.empty())
+        {
+            *out++ = current_piece;
+        }
+        return out;
     }
-    return out;
-}
+};
 
 
 }} // namespace detail::overlay
