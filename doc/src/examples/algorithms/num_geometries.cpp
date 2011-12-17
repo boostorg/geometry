ea621ac5c96eadb4c0876becbd8ee22cc02cdcc2
Barend Gehrels <barend@xs4all.nl>
2011-12-17 11:39:10 +0000

    Fixed multi/wkt examples (in algorithms)

--- doc/src/examples/algorithms/num_geometries.cpp
+++ doc/src/examples/algorithms/num_geometries.cpp
@@ -17,6 +17,7 @@
 #include <boost/geometry/geometries/polygon.hpp>
 #include <boost/geometry/multi/geometries/multi_polygon.hpp>
 #include <boost/geometry/io/wkt/wkt.hpp>
+#include <boost/geometry/multi/io/wkt/wkt.hpp>
 
 
 int main()
