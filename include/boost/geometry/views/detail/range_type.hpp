69c1bbcbb99feb537d98ab1aaba910759f442a15
Barend Gehrels <barend@xs4all.nl>
2012-01-14 22:25:53 +0000

    Added implementation for boxes

--- include/boost/geometry/views/detail/range_type.hpp
+++ include/boost/geometry/views/detail/range_type.hpp
@@ -15,12 +15,14 @@
 #define BOOST_GEOMETRY_VIEWS_DETAIL_RANGE_TYPE_HPP
 
 
+#include <boost/mpl/assert.hpp>
 #include <boost/type_traits.hpp>
 
 #include <boost/geometry/core/ring_type.hpp>
 #include <boost/geometry/core/tag.hpp>
 #include <boost/geometry/core/tags.hpp>
 
+#include <boost/geometry/views/box_view.hpp>
 
 namespace boost { namespace geometry
 {
@@ -34,19 +36,24 @@ namespace dispatch
 template <typename GeometryTag, typename Geometry>
 struct range_type
 {
-    // Even if it is not recognized, define itself as a type.
-    // This enables calling range_type over any range
-    // (not necessarily a geometry)
-
-    // Furthermore, applicable for ring/linestring
-    typedef Geometry type;
+    BOOST_MPL_ASSERT_MSG
+        (
+            false, NOT_OR_NOT_YET_IMPLEMENTED_FOR_THIS_GEOMETRY_TYPE
+            , (types<Geometry>)
+        );
 };
 
 
 template <typename Geometry>
-struct range_type<point_tag, Geometry>
+struct range_type<ring_tag, Geometry>
 {
-    typedef void type;
+    typedef Geometry type;
+};
+
+template <typename Geometry>
+struct range_type<linestring_tag, Geometry>
+{
+    typedef Geometry type;
 };
 
 
@@ -54,6 +61,12 @@ template <typename Geometry>
 struct range_type<polygon_tag, Geometry>
 {
     typedef typename ring_type<Geometry>::type type;
+};
+
+template <typename Geometry>
+struct range_type<box_tag, Geometry>
+{
+    typedef typename box_view<Geometry> type;
 };
 
 
