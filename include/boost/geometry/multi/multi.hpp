c4427fd248a4a0e77c70c1bc2d96ae58f0ec6bfa
Barend Gehrels <barend@xs4all.nl>
2012-01-27 22:55:27 +0000

    Fixed covered_by for multi
    [SVN r76728]

--- include/boost/geometry/multi/multi.hpp
+++ include/boost/geometry/multi/multi.hpp
@@ -31,6 +31,7 @@
 #include <boost/geometry/multi/algorithms/clear.hpp>
 #include <boost/geometry/multi/algorithms/convert.hpp>
 #include <boost/geometry/multi/algorithms/correct.hpp>
+#include <boost/geometry/multi/algorithms/covered_by.hpp>
 #include <boost/geometry/multi/algorithms/distance.hpp>
 #include <boost/geometry/multi/algorithms/envelope.hpp>
 #include <boost/geometry/multi/algorithms/equals.hpp>
