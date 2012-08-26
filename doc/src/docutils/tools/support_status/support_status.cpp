5686170652107521b3189b9ce01399fe6cbfb20f
Bruno Lalande <bruno.lalande@gmail.com>
2012-08-26 23:38:33 +0000

    Integrated unique into support_status.

--- doc/src/docutils/tools/support_status/support_status.cpp
+++ doc/src/docutils/tools/support_status/support_status.cpp
@@ -35,6 +35,7 @@
 #include <boost/geometry/algorithms/overlaps.hpp>
 #include <boost/geometry/algorithms/simplify.hpp>
 #include <boost/geometry/algorithms/transform.hpp>
+#include <boost/geometry/algorithms/unique.hpp>
 #include <boost/geometry/multi/algorithms/append.hpp>
 #include <boost/geometry/multi/algorithms/area.hpp>
 #include <boost/geometry/multi/algorithms/clear.hpp>
@@ -46,6 +47,7 @@
 #include <boost/geometry/multi/algorithms/equals.hpp>
 #include <boost/geometry/multi/algorithms/simplify.hpp>
 #include <boost/geometry/multi/algorithms/transform.hpp>
+#include <boost/geometry/multi/algorithms/unique.hpp>
 #include <boost/geometry/strategies/strategies.hpp>
 
 #include "text_outputter.hpp"
@@ -101,6 +103,7 @@ DECLARE_BINARY_ALGORITHM(expand)
 DECLARE_BINARY_ALGORITHM(overlaps)
 DECLARE_UNARY_ALGORITHM(simplify)
 DECLARE_BINARY_ALGORITHM(transform)
+DECLARE_UNARY_ALGORITHM(unique)
 DECLARE_BINARY_ALGORITHM(within)
 
 
@@ -225,6 +228,7 @@ void support_status()
     test_binary_algorithm<overlaps, all_types, all_types, OutputFactory>("overlaps");
     test_unary_algorithm<simplify, all_types, OutputFactory>("simplify");
     test_binary_algorithm<transform, all_types, all_types, OutputFactory>("transform");
+    test_unary_algorithm<unique, all_types, OutputFactory>("unique");
     test_binary_algorithm<within, all_types, all_types, OutputFactory>("within");
 }
 
