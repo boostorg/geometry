416721e9590824e7ab4dcdd75b654977583ca55b
Barend Gehrels <barend@xs4all.nl>
2012-12-14 22:28:48 +0000

    [geometry] added combinations point/ring, point/polygon, point/multi_polygon for disjoint and intersects (unit tests)

--- test/algorithms/disjoint.cpp
+++ test/algorithms/disjoint.cpp
@@ -105,6 +105,16 @@ void test_all()
     test_disjoint<ring, ring>("within_simplex_rr1", within_simplex[0], within_simplex[1], false);
     test_disjoint<ring, ring>("within_simplex_rr2", within_simplex[1], within_simplex[0], false);
 
+    test_disjoint<P, ring>("point_ring1", "POINT(0 0)", "POLYGON((0 0,3 3,6 0,0 0))", false);
+    test_disjoint<P, ring>("point_ring2", "POINT(3 1)", "POLYGON((0 0,3 3,6 0,0 0))", false);
+    test_disjoint<P, ring>("point_ring3", "POINT(0 3)", "POLYGON((0 0,3 3,6 0,0 0))", true);
+    test_disjoint<P, polygon>("point_polygon1", "POINT(0 0)", "POLYGON((0 0,3 3,6 0,0 0))", false);
+    test_disjoint<P, polygon>("point_polygon2", "POINT(3 1)", "POLYGON((0 0,3 3,6 0,0 0))", false);
+    test_disjoint<P, polygon>("point_polygon3", "POINT(0 3)", "POLYGON((0 0,3 3,6 0,0 0))", true);
+
+    test_disjoint<ring, P>("point_ring2", "POLYGON((0 0,3 3,6 0,0 0))", "POINT(0 0)", false);
+    test_disjoint<polygon, P>("point_polygon2", "POLYGON((0 0,3 3,6 0,0 0))", "POINT(0 0)", false);
+
     // Linear
     typedef bg::model::linestring<P> ls;
     typedef bg::model::segment<P> segment;
