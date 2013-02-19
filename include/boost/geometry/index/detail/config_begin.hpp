27c53a377962c5cee3e53c7b5f79cab4669950ee
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2013-02-19 14:30:44 +0000

    rtree: definition of _CRT_SECURE_NO_DEPRECATE removed
    [SVN r83001]

--- include/boost/geometry/index/detail/config_begin.hpp
+++ include/boost/geometry/index/detail/config_begin.hpp
@@ -12,11 +12,6 @@
 
     #define BOOST_GEOMETRY_INDEX_DETAIL_USE_PARAM(A) (void)A;
 
-    #ifndef _CRT_SECURE_NO_DEPRECATE
-    #define BOOST_GEOMETRY_INDEX_DETAIL_CRT_SECURE_NO_DEPRECATE_ENABLED
-    #define _CRT_SECURE_NO_DEPRECATE
-    #endif
-
     #pragma warning (push)
     #pragma warning (disable : 4512) // assignment operator could not be generated
     #pragma warning (disable : 4127) // conditional expression is constant
