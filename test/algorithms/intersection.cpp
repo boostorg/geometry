5777e0b7ba06cea97a1e1001118c1118fb2a0d5d
Barend Gehrels <barend@xs4all.nl>
2013-03-17 17:20:47 +0000

    [geometry] added case for ticket 8254, which had the same problem as buffer_mp1

--- test/algorithms/intersection.cpp
+++ test/algorithms/intersection.cpp
@@ -213,6 +213,8 @@ void test_areal()
     test_one<Polygon, Polygon, Polygon>("buffer_rt_g", buffer_rt_g[0], buffer_rt_g[1],
                 1, 0, 2.914213562373);
 
+    test_one<Polygon, Polygon, Polygon>("ticket_8254", ticket_8254[0], ticket_8254[1],
+                1, 4, 3.63593e-08, 0.01);
 
     test_one<Polygon, Polygon, Polygon>("buffer_mp1", buffer_mp1[0], buffer_mp1[1],
                 1, 31, 2.271707796);
