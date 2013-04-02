d490cec1511edaf558d5d1d40392d467a8d98ac4
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2013-04-02 14:00:57 +0000

    rtree: fixed error in move assignment (allocators comparison), some comments added
    [SVN r83709]

--- include/boost/geometry/index/detail/utilities.hpp
+++ include/boost/geometry/index/detail/utilities.hpp
@@ -32,29 +32,6 @@ static inline void move_cond(T & l, T & r, boost::mpl::bool_<true> const&)
 template<class T>
 static inline void move_cond(T &, T &, boost::mpl::bool_<false> const&) {}
 
-// Darwin compiler (tested on 4.2.1) can't copy empty base classes properly!
-// Consider using following functions:
-
-//template <typename T> inline
-//void swap_cond_impl(T &, T &, boost::true_type const& /*is_empty*/) {}
-//
-//template <typename T> inline
-//void swap_cond_impl(T & l, T & r, boost::false_type const& /*is_empty*/)
-//{
-//    ::boost::swap(l, r);
-//}
-//
-//template <typename T> inline
-//void swap_cond(T & l, T & r, boost::mpl::bool_<true> const&)
-//{
-//#ifdef BOOST_IS_EMPTY
-//    typedef typename ::boost::is_empty<T>::type is_empty;
-//#else
-//    typedef ::boost::false_type is_empty;
-//#endif
-//    swap_cond_impl(l, r, is_empty());
-//}
-
 template <typename T> inline
 void swap_cond(T & l, T & r, boost::mpl::bool_<true> const&)
 {
