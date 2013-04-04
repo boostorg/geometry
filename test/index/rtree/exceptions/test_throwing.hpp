476c95e1be7aa641794d824dbadd3a2f5952e2ba
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2013-04-04 20:07:20 +0000

    geometry.index test: functions closed in namespaces.

--- test/index/rtree/exceptions/test_throwing.hpp
+++ test/index/rtree/exceptions/test_throwing.hpp
@@ -62,8 +62,9 @@ struct throwing_value
     int value;
 };
 
+namespace generate {
 template <typename T, typename C>
-struct generate_value< std::pair<bg::model::point<T, 2, C>, throwing_value> >
+struct value< std::pair<bg::model::point<T, 2, C>, throwing_value> >
 {
     typedef bg::model::point<T, 2, C> P;
     typedef std::pair<P, throwing_value> R;
@@ -72,74 +73,7 @@ struct generate_value< std::pair<bg::model::point<T, 2, C>, throwing_value> >
         return std::make_pair(P(x, y), throwing_value(x + y * 100));
     }
 };
-
-// box
-//
-//#include <boost/geometry/geometries/register/box.hpp>
-//
-//struct throwing_box_copy_exception : public std::exception
-//{
-//    const char * what() const throw() { return "box copy failed."; }
-//};
-//
-//template <typename Point>
-//struct throwing_box
-//{
-//    throwing_box(){}
-//
-//    throwing_box(Point const& m, Point const& mm)
-//        : min_p(m), max_p(mm)
-//    {}
-//
-//    throwing_box(throwing_box const& o)
-//    {
-//        throw_if_required();
-//
-//        min_p = o.min_p;
-//        max_p = o.max_p;
-//    }
-//
-//    throwing_box & operator=(throwing_box const& o)
-//    {
-//        throw_if_required();
-//
-//        min_p = o.min_p;
-//        max_p = o.max_p;
-//        return *this;
-//    }
-//
-//    void throw_if_required()
-//    {
-//        // throw if counter meets max count
-//        if ( get_max_calls_ref() <= get_calls_counter_ref() )
-//            throw throwing_box_copy_exception();
-//        else
-//            ++get_calls_counter_ref();
-//    }
-//
-//    static void reset_calls_counter() { get_calls_counter_ref() = 0; }
-//    static void set_max_calls(size_t mc) { get_max_calls_ref() = mc; }
-//
-//    static size_t & get_calls_counter_ref() { static size_t cc = 0; return cc; }
-//    static size_t & get_max_calls_ref() { static size_t mc = (std::numeric_limits<size_t>::max)(); return mc; }
-//
-//    Point min_p;
-//    Point max_p;
-//};
-//
-//BOOST_GEOMETRY_REGISTER_BOX_TEMPLATED(throwing_box, min_p, max_p)
-//
-//namespace boost { namespace geometry { namespace index {
-//
-//template <typename CT, size_t D, typename CS>
-//struct default_box_type< bg::model::point<CT, D, CS> >
-//{
-//    typedef throwing_box<
-//        bg::model::point<CT, D, CS>
-//    > type;
-//};
-//
-//}}} // namespace boost::geometry::index
+} // namespace generate
 
 #include <boost/geometry/index/detail/varray.hpp>
 
