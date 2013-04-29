f61cbbd01c9ec433b2fbba2b6c0538a246d8f402
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2013-04-29 20:51:25 +0000

    geometry.index test: default parameters used in rstar exceptions test.
    [SVN r84088]

--- index/test/rtree/exceptions/rtree_exceptions_rst.cpp
+++ index/test/rtree/exceptions/rtree_exceptions_rst.cpp
@@ -7,14 +7,16 @@
 // Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
+#include <Windows.h>
+
 #include <rtree/exceptions/test_exceptions.hpp>
 
 int test_main(int, char* [])
 {
-    test_rtree_value_exceptions< bgi::rstar<4, 2, 0, 2> >();
-    test_rtree_value_exceptions(bgi::dynamic_rstar(4, 2, 0, 2));
+    test_rtree_value_exceptions< bgi::rstar<4, 2> >();
+    test_rtree_value_exceptions(bgi::dynamic_rstar(4, 2));
 
-    test_rtree_elements_exceptions< bgi::rstar_throwing<4, 2, 0, 2> >();
+    test_rtree_elements_exceptions< bgi::rstar_throwing<4, 2> >();
 
     return 0;
 }
