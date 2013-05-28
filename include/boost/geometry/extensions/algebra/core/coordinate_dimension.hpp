af7896216e4c9ea6c91e02bf3ac85e305e10bf7f
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2013-05-28 22:45:57 +0000

    geometry extensions: Added Vector concept, implemented assign(), clear(), num_points.hpp(), implemented translations for Point and Box. Fixed includes in extensions/ball.

--- include/boost/geometry/extensions/algebra/core/coordinate_dimension.hpp
+++ include/boost/geometry/extensions/algebra/core/coordinate_dimension.hpp
@@ -3,6 +3,7 @@
 // Copyright (c) 2007-2012 Barend Gehrels, Amsterdam, the Netherlands.
 // Copyright (c) 2008-2012 Bruno Lalande, Paris, France.
 // Copyright (c) 2009-2012 Mateusz Loskot, London, UK.
+// Copyright (c) 2013 Adam Wulkiewicz, Lodz, Poland.
 
 // Parts of Boost.Geometry are redesigned from Geodan's Geographic Library
 // (geolib/GGL), copyright (c) 1995-2010 Geodan, Amsterdam, the Netherlands.
@@ -11,37 +12,26 @@
 // Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
-#ifndef BOOST_GEOMETRY_EXTENSIONS_BALL_ALGORITHMS_CLEAR_HPP
-#define BOOST_GEOMETRY_EXTENSIONS_BALL_ALGORITHMS_CLEAR_HPP
+#ifndef BOOST_GEOMETRY_EXTENSIONS_ALGEBRA_CORE_COORDINATE_DIMENSION_HPP
+#define BOOST_GEOMETRY_EXTENSIONS_ALGEBRA_CORE_COORDINATE_DIMENSION_HPP
 
+#include <boost/geometry/core/coordinate_system.hpp>
 
-#include <boost/geometry/algorithms/clear.hpp>
+#include <boost/geometry/extensions/algebra/core/tags.hpp>
 
-#include <boost/geometry/extensions/ball/core/tags.hpp>
-
-
-
-namespace boost { namespace geometry
-{
-
+namespace boost { namespace geometry {
 
 #ifndef DOXYGEN_NO_DISPATCH
-namespace dispatch
-{
+namespace core_dispatch {
 
-
-
-template <typename Geometry>
-struct clear<ball_tag, Geometry>
-    : detail::clear::no_action<Geometry>
+template <typename V>
+struct dimension<vector_tag, V>
+    : traits::dimension<typename geometry::util::bare_type<V>::type>
 {};
 
-
-
-} // namespace dispatch
+} // namespace core_dispatch
 #endif // DOXYGEN_NO_DISPATCH
-
 
 }} // namespace boost::geometry
 
-#endif // BOOST_GEOMETRY_EXTENSIONS_BALL_ALGORITHMS_CLEAR_HPP
+#endif // BOOST_GEOMETRY_EXTENSIONS_ALGEBRA_CORE_COORDINATE_DIMENSION_HPP
