bd79d311c92d2237009f1731c7e00ff511f48b50
Barend Gehrels <barend@xs4all.nl>
2013-08-26 20:18:27 +0000

    [geometry] avoid clang warning about unused ublas function
    [SVN r85478]

--- include/boost/geometry/strategies/transform/matrix_transformers.hpp
+++ include/boost/geometry/strategies/transform/matrix_transformers.hpp
@@ -22,8 +22,19 @@
 #define BOOST_UBLAS_TYPE_CHECK 0
 
 #include <boost/numeric/conversion/cast.hpp>
+
+#if defined(__clang__)
+// Avoid warning about unused UBLAS function: boost_numeric_ublas_abs
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-function"
+#endif
+
 #include <boost/numeric/ublas/vector.hpp>
 #include <boost/numeric/ublas/matrix.hpp>
+
+#if defined(__clang__)
+#pragma clang diagnostic pop
+#endif
 
 #include <boost/geometry/core/access.hpp>
 #include <boost/geometry/core/coordinate_dimension.hpp>
