ce263c396878f9bbf26f1b4d131ddecb1c918946
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2013-12-11 12:07:27 +0100

    [geometry] added some tests for touches(Ls,Ls)

--- test/algorithms/touches.cpp
+++ test/algorithms/touches.cpp
@@ -176,6 +176,8 @@ void test_all()
 
     test_touches<linestring, linestring>("LINESTRING(0 0,1 1,2 2)", "LINESTRING(2 0,2 2,1 2,1 1)", true);
     test_touches<linestring, linestring>("LINESTRING(2 2,1 1,0 0)", "LINESTRING(2 0,2 2,1 2,1 1)", true);
+    test_touches<linestring, linestring>("LINESTRING(0 0,1 1,2 2)", "LINESTRING(1 1,1 2,2 2,2 0)", true);
+    test_touches<linestring, linestring>("LINESTRING(2 2,1 1,0 0)", "LINESTRING(1 1,1 2,2 2,2 0)", true);
 }
 
 
