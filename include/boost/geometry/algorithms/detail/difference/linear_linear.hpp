c5048bd8f931f0a925531eb4cefc915ab0210d98
Menelaos Karavelas <menelaos.karavelas@gmail.com>
2014-02-20 13:37:43 +0200

    updated implementation so that it works for the intersection of linear geometries

--- include/boost/geometry/algorithms/detail/difference/linear_linear.hpp
+++ include/boost/geometry/algorithms/detail/difference/linear_linear.hpp
@@ -8,8 +8,8 @@
 // Contributed and/or modified by Menelaos Karavelas, on behalf of Oracle
 
 
-#ifndef BOOST_GEOMETRY_ALGORITHMS_DETAIL_DIFFERENCE_LINEAR_LINEAR_HPP
-#define BOOST_GEOMETRY_ALGORITHMS_DETAIL_DIFFERENCE_LINEAR_LINEAR_HPP
+#ifndef BOOST_GEOMETRY_ALGORITHMS_DETAIL_OVERLAY_LINEAR_LINEAR_HPP
+#define BOOST_GEOMETRY_ALGORITHMS_DETAIL_OVERLAY_LINEAR_LINEAR_HPP
 
 #include <algorithm>
 
@@ -35,12 +35,21 @@ namespace detail { namespace difference
 //===========================================================================
 
 
-template <typename LineStringOut, typename Geometry, typename GeometryTag>
-struct difference_no_intersections;
+template
+<
+    typename LineStringOut,
+    overlay_type OverlayType,
+    typename Geometry,
+    typename GeometryTag
+>
+struct linear_linear_no_intersections;
 
 
 template <typename LineStringOut, typename LineString>
-struct difference_no_intersections<LineStringOut, LineString, linestring_tag>
+struct linear_linear_no_intersections
+    <
+        LineStringOut, overlay_difference, LineString, linestring_tag
+    >
 {
     template <typename OutputIterator>
     static inline OutputIterator apply(LineString const& linestring,
@@ -55,9 +64,12 @@ struct difference_no_intersections<LineStringOut, LineString, linestring_tag>
 
 
 template <typename LineStringOut, typename MultiLineString>
-struct difference_no_intersections
+struct linear_linear_no_intersections
     <
-        LineStringOut, MultiLineString, multi_linestring_tag
+        LineStringOut,
+        overlay_difference,
+        MultiLineString,
+        multi_linestring_tag
     >
 {
     template <typename OutputIterator>
@@ -75,7 +87,32 @@ struct difference_no_intersections
     }
 };
 
-template <typename LinestringOut>
+
+template <typename LineStringOut, typename Geometry, typename GeometryTag>
+struct linear_linear_no_intersections
+    <
+        LineStringOut, overlay_intersection, Geometry, GeometryTag
+    >
+{
+    template <typename OutputIterator>
+    static inline OutputIterator apply(Geometry const&,
+                                       OutputIterator oit)
+    {
+        return oit;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+template <typename LinestringOut, overlay_type OverlayType>
 struct linear_linear_linestring
 {
     typedef typename point_type<LinestringOut>::type PointOut;
@@ -215,9 +252,12 @@ struct linear_linear_linestring
 #endif
             // MK:: need to think about this
             //            std::copy(linear1.begin(), linear1.end(), oit);
-            oit = difference_no_intersections
+            oit = linear_linear_no_intersections
                 <
-                    LinestringOut, Linear1, typename tag<Linear1>::type
+                    LinestringOut,
+                    OverlayType,
+                    Linear1,
+                    typename tag<Linear1>::type
                 >::apply(linear1, oit);
             return oit;
         }
@@ -248,272 +288,10 @@ struct linear_linear_linestring
                 LinestringOut,
                 Linear1,
                 Linear2,
-                overlay_difference
+                OverlayType
             >::apply(linear1, linear2, turns, reverse_turns, oit);
     }
 };
-
-
-
-//===========================================================================
-//===========================================================================
-//===========================================================================
-
-
-
-template <typename LinestringOut>
-struct multilinestring_multilinestring_linestring
-{
-    typedef typename point_type<LinestringOut>::type PointOut;
-    typedef overlay::traversal_turn_info<PointOut> turn_info;
-    typedef std::vector<turn_info> Turns;
-    typedef typename Turns::iterator TurnIt;
-    typedef detail::get_turns::no_interrupt_policy InterruptPolicy;
-
-
-    struct AssignPolicy
-    {
-        static bool const include_no_turn = false;
-        static bool const include_degenerate = false;
-        static bool const include_opposite = false;
-
-        template
-        <
-            typename Info,
-            typename Point1,
-            typename Point2,
-            typename IntersectionInfo,
-            typename DirInfo
-        >
-        static inline void apply(Info& info, Point1 const& p1, Point2 const& p2,
-                                 IntersectionInfo const& ii, DirInfo const& di)
-        {
-            overlay::calculate_distance_policy::apply(info, p1, p2, ii, di);
-        }
-    };
-
-
-    struct IsContinueTurn
-    {
-        template <typename Turn>
-        bool operator()(Turn const& turn) const
-        {
-            if ( turn.method != overlay::method_collinear &&
-                 turn.method != overlay::method_equal )
-            {
-                return false;
-            }
-            overlay::operation_type op[2];
-            op[0] = turn.operations[0].operation;
-            op[1] = turn.operations[1].operation;
-
-            return
-                (op[0] == overlay::operation_continue ||
-                 op[0] == overlay::operation_opposite) &&
-                (op[1] == overlay::operation_continue ||
-                 op[1] == overlay::operation_opposite);
-        }
-    };
-
-
-    template <typename Turns>
-    static inline void filter_turns(Turns& turns)
-    {
-        typedef typename Turns::iterator TurnIt;
-
-        TurnIt new_end = std::remove_if(turns.begin(), turns.end(),
-                                        IsContinueTurn());
-        turns.resize( std::distance(turns.begin(), new_end) );
-    }
-
-
-    template
-    <
-        typename Linestring1, typename Linestring2,
-        typename OutputIterator, typename Strategy
-    >
-    static inline OutputIterator apply(Linestring1 const& multilinestring1,
-                                       Linestring2 const& multilinestring2,
-                                       OutputIterator oit,
-                                       Strategy const& )
-    {
-        typedef geometry::model::multi_linestring
-            <
-                LinestringOut
-            > MultiLinestringOut;
-
-        MultiLinestringOut mls1, mls2;
-        geometry::convert(multilinestring1, mls1);
-        geometry::convert(multilinestring2, mls2);
-
-        assert( boost::size(mls1) > 0 );
-        assert( boost::size(mls2) > 0 );
-
-
-        //        canonical<LinestringOut>::apply(ls1);
-        //        canonical<LinestringOut>::apply(ls2);
-
-        //        typedef typename point_type<LinestringOut>::type PointOut;
-
-#if 0
-        typedef //overlay::assign_null_policy
-            overlay::calculate_distance_policy AssignPolicy;
-#endif
-        //        typedef //overlay::assign_null_policy
-        //            detail::union_::assign_union_policy AssignPolicy;
-
-        //        typedef detail::disjoint::disjoint_interrupt_policy InterruptPolicy;
-
-        Turns turns, reverse_turns;
-
-        geometry::detail::relate::turns::get_turns
-            <
-                MultiLinestringOut,
-                MultiLinestringOut,
-                detail::get_turns::get_turn_info_type
-                <
-                    MultiLinestringOut,
-                    MultiLinestringOut,
-                    AssignPolicy
-                >
-            >::apply(turns, mls1, mls2);
-
-        MultiLinestringOut mls2_reverse = mls2;
-        geometry::reverse(mls2_reverse);
-        geometry::detail::relate::turns::get_turns
-            <
-                MultiLinestringOut,
-                MultiLinestringOut,
-                detail::get_turns::get_turn_info_type
-                <
-                    MultiLinestringOut,
-                    MultiLinestringOut,
-                    AssignPolicy
-                >
-            >::apply(reverse_turns, mls1, mls2_reverse);
-
-        if ( turns.empty() )
-        {
-            // the two linestrings are disjoint; we return the first as is;
-            //            canonical<MultiLinestringOut>::apply(mls1);
-#ifdef GEOMETRY_TEST_DEBUG
-            std::cout << "NO INTERSECTIONS" << std::endl;
-#endif
-            std::copy(mls1.begin(), mls1.end(), oit);
-            return oit;
-        }
-
-        // remove turns that have no added value
-#if 1
-        filter_turns(turns);
-        filter_turns(reverse_turns);
-#endif
-
-        // sort by seg_id, distance, and operation
-        typedef detail::turns::less_seg_dist_other_op<> less;
-        std::sort(boost::begin(turns), boost::end(turns), less());
-
-        typedef
-        detail::turns::less_seg_dist_other_op<std::greater<int> > rev_less;
-        std::sort(boost::begin(reverse_turns), boost::end(reverse_turns),
-                  rev_less());
-
-#ifdef GEOMETRY_TEST_DEBUG
-        detail::turns::print_turns(mls1, mls2, turns);
-        std::cout << std::endl << std::endl;
-        detail::turns::print_turns(mls1, mls2_reverse, reverse_turns);
-#endif
-
-        return detail::turns::following::follow
-            <
-                LinestringOut,
-                MultiLinestringOut,
-                MultiLinestringOut,
-                overlay_difference
-            >::apply(mls1, mls2, turns, reverse_turns, oit);
-    }
-};
-
-
-//===========================================================================
-//===========================================================================
-//===========================================================================
-
-
-
-
-
-template <typename LinestringOut>
-struct linestring_linestring_linestring
-{
-    typedef typename point_type<LinestringOut>::type PointOut;
-    typedef overlay::traversal_turn_info<PointOut> turn_info;
-
-
-    template
-    <
-        typename Linestring1, typename Linestring2,
-        typename OutputIterator, typename Strategy
-    >
-    static inline OutputIterator apply(Linestring1 const& linestring1,
-                                       Linestring2 const& linestring2,
-                                       OutputIterator oit,
-                                       Strategy const& strategy)
-    {
-        geometry::model::multi_linestring<Linestring1> mls1;
-        geometry::model::multi_linestring<Linestring2> mls2;
-        
-        mls1.push_back(linestring1);
-        mls2.push_back(linestring2);
-
-        
-        return
-            multilinestring_multilinestring_linestring
-            <
-                LinestringOut
-            >::apply(mls1, mls2, oit, strategy);
-    }
-};
-
-
-//===========================================================================
-//===========================================================================
-//===========================================================================
-
-
-
-
-
-template <typename LinestringOut>
-struct linestring_multilinestring_linestring
-{
-    typedef typename point_type<LinestringOut>::type PointOut;
-    typedef overlay::traversal_turn_info<PointOut> turn_info;
-
-
-    template
-    <
-        typename Linestring, typename MultiLinestring,
-        typename OutputIterator, typename Strategy
-    >
-    static inline OutputIterator apply(Linestring const& linestring,
-                                       MultiLinestring const& multilinestring,
-                                       OutputIterator oit,
-                                       Strategy const& strategy)
-    {
-        geometry::model::multi_linestring<Linestring> mls;
-        
-        mls.push_back(linestring);
-        
-        return
-            multilinestring_multilinestring_linestring
-            <
-                LinestringOut
-            >::apply(mls, multilinestring, oit, strategy);
-    }
-};
-
-
 
 
 }} // namespace detail::difference
@@ -524,4 +302,4 @@ struct linestring_multilinestring_linestring
 
 
 
-#endif // BOOST_GEOMETRY_ALGORITHMS_DETAIL_DIFFERENCE_LINEAR_LINEAR_HPP
+#endif // BOOST_GEOMETRY_ALGORITHMS_DETAIL_OVERLAY_LINEAR_LINEAR_HPP
