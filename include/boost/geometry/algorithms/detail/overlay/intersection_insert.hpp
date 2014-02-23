671a28aed45fed015ab9c71fc24c6984ebec0947
Menelaos Karavelas <menelaos.karavelas@gmail.com>
2014-02-23 11:48:50 +0200

    updated copyright year for Barend;

--- include/boost/geometry/algorithms/detail/overlay/intersection_insert.hpp
+++ include/boost/geometry/algorithms/detail/overlay/intersection_insert.hpp
@@ -1,6 +1,6 @@
 // Boost.Geometry (aka GGL, Generic Geometry Library)
 
-// Copyright (c) 2007-2012 Barend Gehrels, Amsterdam, the Netherlands.
+// Copyright (c) 2007-2014 Barend Gehrels, Amsterdam, the Netherlands.
 
 // This file was modified by Oracle on 2014.
 // Modifications copyright (c) 2014 Oracle and/or its affiliates.
