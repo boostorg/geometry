caadd6ce000ace4e28ae79d108bbc55cb2078a1a
Menelaos Karavelas <menelaos.karavelas@gmail.com>
2014-04-10 08:45:38 +0300

    [set ops] replace included unit test by unit test

--- test/algorithms/difference_linear_linear.cpp
+++ test/algorithms/difference_linear_linear.cpp
@@ -18,7 +18,7 @@
 #define BOOST_GEOMETRY_DEBUG_SEGMENT_IDENTIFIER
 #endif
 
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 
 #include "test_difference_linear_linear.hpp"
 
