f32fb58b9ddfb534be6e25c3c59f324a06729468
Menelaos Karavelas <menelaos.karavelas@gmail.com>
2014-05-28 12:47:09 +0300

    Revert "[overlay] fix comparison between signed/unsigned integer expressions warning"
    This reverts commit ad2a56cfa9bf2289a4076be55dee13e2e28a59a8.

--- include/boost/geometry/algorithms/detail/overlay/append_no_dups_or_spikes.hpp
+++ include/boost/geometry/algorithms/detail/overlay/append_no_dups_or_spikes.hpp
@@ -96,13 +96,11 @@ template <typename Range, typename RobustPolicy>
 inline void clean_closing_dups_and_spikes(Range& range,
                 RobustPolicy const& robust_policy)
 {
-    typename boost::range_size<Range>::type const minsize = static_cast
-        <
-            typename boost::range_size<Range>::type
-        >(core_detail::closure::minimum_ring_size
+    int const minsize
+        = core_detail::closure::minimum_ring_size
             <
                 geometry::closure<Range>::value
-            >::value);
+            >::value;
 
     if (boost::size(range) <= minsize)
     {
