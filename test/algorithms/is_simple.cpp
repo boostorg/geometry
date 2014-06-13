50826192c68ea982906e410738c0e008ab9c68d7
Menelaos Karavelas <menelaos.karavelas@gmail.com>
2014-06-13 16:22:28 +0300

    [test][algorithms] polish copyright header

--- test/algorithms/is_simple.cpp
+++ test/algorithms/is_simple.cpp
@@ -1,11 +1,12 @@
 // Boost.Geometry (aka GGL, Generic Geometry Library)
+// Unit Test
 
 // Copyright (c) 2014, Oracle and/or its affiliates.
 
+// Contributed and/or modified by Menelaos Karavelas, on behalf of Oracle
+
 // Licensed under the Boost Software License version 1.0.
 // http://www.boost.org/users/license.html
-
-// Contributed and/or modified by Menelaos Karavelas, on behalf of Oracle
 
 #include <iostream>
 
