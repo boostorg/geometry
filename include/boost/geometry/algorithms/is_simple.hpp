dbbcf4c1ff9582eea18d0da7956fab5bd7b32290
Menelaos Karavelas <menelaos.karavelas@gmail.com>
2014-06-18 14:04:50 +0300

    [algorithms][is_simple] implement the new directory/file structure

--- include/boost/geometry/algorithms/is_simple.hpp
+++ include/boost/geometry/algorithms/is_simple.hpp
@@ -10,66 +10,7 @@
 #ifndef BOOST_GEOMETRY_ALGORITHMS_IS_SIMPLE_HPP
 #define BOOST_GEOMETRY_ALGORITHMS_IS_SIMPLE_HPP
 
-#include <boost/variant/static_visitor.hpp>
-#include <boost/variant/apply_visitor.hpp>
-#include <boost/variant/variant_fwd.hpp>
-
-#include <boost/geometry/algorithms/detail/is_simple/always_simple.hpp>
-#include <boost/geometry/algorithms/detail/is_simple/areal.hpp>
-#include <boost/geometry/algorithms/detail/is_simple/linear.hpp>
-#include <boost/geometry/algorithms/detail/is_simple/multipoint.hpp>
-
-#include <boost/geometry/algorithms/dispatch/is_simple.hpp>
-
-
-namespace boost { namespace geometry
-{
-
-
-namespace resolve_variant {
-
-template <typename Geometry>
-struct is_simple
-{
-    static inline bool apply(Geometry const& geometry)
-    {
-        concept::check<Geometry const>();
-        return dispatch::is_simple<Geometry>::apply(geometry);
-    }
-};
-
-template <BOOST_VARIANT_ENUM_PARAMS(typename T)>
-struct is_simple<boost::variant<BOOST_VARIANT_ENUM_PARAMS(T)> >
-{
-    struct visitor : boost::static_visitor<bool>
-    {
-        template <typename Geometry>
-        bool operator()(Geometry const& geometry) const
-        {
-            return is_simple<Geometry>::apply(geometry);
-        }
-    };
-
-    static inline bool
-    apply(boost::variant<BOOST_VARIANT_ENUM_PARAMS(T)> const& geometry)
-    {
-        return boost::apply_visitor(visitor(), geometry);
-    }
-};
-
-} // namespace resolve_variant
-
-
-
-template <typename Geometry>
-inline bool is_simple(Geometry const& g)
-{
-    return resolve_variant::is_simple<Geometry>::apply(g);
-}
-
-
-}} // namespace boost::geometry
-
-
+#include <boost/geometry/algorithms/detail/is_simple/interface.hpp>
+#include <boost/geometry/algorithms/detail/is_simple/implementation.hpp>
 
 #endif // BOOST_GEOMETRY_ALGORITHMS_IS_SIMPLE_HPP
