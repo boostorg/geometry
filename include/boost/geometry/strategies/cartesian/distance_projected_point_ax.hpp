c5241dd8ee682c28774459461b248c5aab42e9ee
Barend Gehrels <barend@xs4all.nl>
2014-07-14 21:54:01 +0200

    [distance] added missing include file

--- include/boost/geometry/strategies/cartesian/distance_projected_point_ax.hpp
+++ include/boost/geometry/strategies/cartesian/distance_projected_point_ax.hpp
@@ -38,6 +38,7 @@
 #include <boost/geometry/strategies/distance.hpp>
 #include <boost/geometry/strategies/default_distance_result.hpp>
 #include <boost/geometry/strategies/cartesian/distance_pythagoras.hpp>
+#include <boost/geometry/strategies/cartesian/distance_projected_point.hpp>
 
 #include <boost/geometry/util/select_coordinate_type.hpp>
 
