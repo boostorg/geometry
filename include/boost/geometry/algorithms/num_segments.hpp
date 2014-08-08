b0c06af00b672b6cbd25630a9b1863fee7030921
Menelaos Karavelas <menelaos.karavelas@gmail.com>
2014-08-08 20:50:51 +0300

    [algorithms][num_segments] replace unnecessary "typename" by "inline"

--- include/boost/geometry/algorithms/num_segments.hpp
+++ include/boost/geometry/algorithms/num_segments.hpp
@@ -172,7 +172,7 @@ struct num_segments<boost::variant<BOOST_VARIANT_ENUM_PARAMS(T)> >
         visitor(bool add_for_open): m_add_for_open(add_for_open) {}
 
         template <typename Geometry>
-        typename std::size_t operator()(Geometry const& geometry) const
+        inline std::size_t operator()(Geometry const& geometry) const
         {
             return num_segments<Geometry>::apply(geometry, m_add_for_open);
         }
