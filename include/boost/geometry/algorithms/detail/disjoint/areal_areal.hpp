4a097311c9fd6873a63b3fd9b131adbd848cd38f
Matt Amos <zerebubuth@gmail.com>
2014-10-16 18:11:13 +0100

    [disjoint] Fixed bugs with multipolygons and touching points.
    Multipolygons: Tests were failing when the second part of a
    multipolygon intersected, as `point_on_border` returns only a
    single point. The check now tests all parts of a multipolygon.
    
    Touching points: Previously, it was checking if a point on one
    geometry was within the other, but this was not sufficient if the
    outer rings touched. The within check would fail, as the point
    may be on the boundary rather than strictly within. The code now
    uses `covered_by` and passes the tests.

--- include/boost/geometry/algorithms/detail/disjoint/areal_areal.hpp
+++ include/boost/geometry/algorithms/detail/disjoint/areal_areal.hpp
@@ -23,7 +23,7 @@
 
 #include <boost/geometry/core/point_type.hpp>
 
-#include <boost/geometry/algorithms/within.hpp>
+#include <boost/geometry/algorithms/covered_by.hpp>
 #include <boost/geometry/algorithms/detail/for_each_range.hpp>
 #include <boost/geometry/algorithms/detail/point_on_border.hpp>
 
@@ -38,18 +38,78 @@ namespace boost { namespace geometry
 namespace detail { namespace disjoint
 {
 
+template <typename FirstGeometry, typename SecondGeometry>
+inline bool rings_containing(FirstGeometry const& geometry1,
+                             SecondGeometry const& geometry2);
+
+namespace dispatch_multi {
+// Given that geometry1 and geometry2 have no intersecting line-work, then
+// return true if geometry1 contains a point in geometry2.
+//
+// For intersecting Polygons one's exterior ring must be within the other one
+// (or the other one containing the first one, same situation) and it musn't
+// be contained entirely inside a hole. If those conditions are met, they
+// must be intersecting each other.
+template <typename FirstGeometry, typename SecondGeometry, typename SecondGeomTag>
+struct rings_containing_impl
+{
+    inline static bool apply(
+        FirstGeometry const& geometry1,
+        SecondGeometry const& geometry2)
+    {
+        typedef typename point_type<SecondGeometry>::type point_type;
+        point_type point;
+        geometry::point_on_border(point, geometry2);
+        return geometry::covered_by(point, geometry1);
+    }
+};
+
+template <typename FirstGeometry, typename SecondGeometry>
+struct rings_containing_impl<FirstGeometry, SecondGeometry, multi_polygon_tag>
+{
+    inline static bool apply(
+        FirstGeometry const& geometry1,
+        SecondGeometry const& geometry2)
+    {
+        for (typename boost::range_iterator<const SecondGeometry>::type
+                 it = boost::begin(geometry2); it != boost::end(geometry2); ++it)
+        {
+            if (disjoint::rings_containing(geometry1, *it))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+// Need to know whether the second geometry, which we'll be generating test
+// points for, is a multipolygon or not. If it's a multipolygon, we'll need
+// to test all the outers to ensure that the test is valid.
+template
+<
+    typename FirstGeometry,
+    typename SecondGeometry,
+    typename SecondGeomTag = typename tag<SecondGeometry>::type
+>
+struct rings_containing {
+    inline static bool apply(FirstGeometry const& geometry1,
+                             SecondGeometry const& geometry2)
+    {
+        return rings_containing_impl<FirstGeometry, SecondGeometry, SecondGeomTag>
+          ::apply(geometry1, geometry2);
+    }
+};
+
+} // namespace dispatch_multi
 
 template <typename FirstGeometry, typename SecondGeometry>
 inline bool rings_containing(FirstGeometry const& geometry1,
-                                 SecondGeometry const& geometry2)
+                             SecondGeometry const& geometry2)
 {
-    typedef typename point_type<SecondGeometry>::type point_type;
-    point_type point;
-    geometry::point_on_border<point_type, SecondGeometry>(point, geometry2);
-    return geometry::within(point, geometry1);
+    typedef dispatch_multi::rings_containing<FirstGeometry, SecondGeometry> rc;
+    return rc::apply(geometry1, geometry2);
 }
-
-
 
 template <typename Geometry1, typename Geometry2>
 struct general_areal
@@ -100,7 +160,6 @@ template <typename Areal, typename Box>
 struct disjoint<Areal, Box, 2, areal_tag, box_tag, false>
     : detail::disjoint::general_areal<Areal, Box>
 {};
-
 
 } // namespace dispatch
 #endif // DOXYGEN_NO_DISPATCH
