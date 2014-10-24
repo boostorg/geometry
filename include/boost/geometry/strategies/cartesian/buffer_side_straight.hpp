a57e8ffe7f4f2e1e16933967e8df8504f4b587d8
Barend Gehrels <barend@xs4all.nl>
2014-10-24 12:36:44 +0200

    [buffer] make constant local variables const

--- include/boost/geometry/strategies/cartesian/buffer_side_straight.hpp
+++ include/boost/geometry/strategies/cartesian/buffer_side_straight.hpp
@@ -67,8 +67,8 @@ public :
         // Generate a block along (left or right of) the segment
 
         // Simulate a vector d (dx,dy)
-        coordinate_type dx = get<0>(input_p2) - get<0>(input_p1);
-        coordinate_type dy = get<1>(input_p2) - get<1>(input_p1);
+        coordinate_type const dx = get<0>(input_p2) - get<0>(input_p1);
+        coordinate_type const dy = get<1>(input_p2) - get<1>(input_p1);
 
         // For normalization [0,1] (=dot product d.d, sqrt)
         promoted_type const length = geometry::math::sqrt(dx * dx + dy * dy);
