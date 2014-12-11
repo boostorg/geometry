936d52db271199a3ea6cb1f00adbd3ab2ad9d2c0
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2014-12-11 22:59:52 +0100

    [index][test] Fix storing of pairs and tuples containing pointers to Geometries.

--- index/test/rtree/rtree_values.cpp
+++ index/test/rtree/rtree_values.cpp
@@ -9,8 +9,10 @@
 
 #include <rtree/test_rtree.hpp>
 
+#include <boost/core/addressof.hpp>
 
 #include <boost/geometry/geometries/register/point.hpp>
+#include <boost/geometry/geometries/polygon.hpp>
 
 struct point
 {
@@ -54,6 +56,33 @@ void test_pair()
     BOOST_CHECK( rt.size() == 0 );
 }
 
+template <typename Box, typename Params>
+void test_pair_geom_ptr()
+{
+    typedef typename bg::point_type<Box>::type point_t;
+    typedef bg::model::polygon<point_t> polygon_t;
+
+    typedef std::pair<Box, polygon_t*> Value;
+
+    typename boost::remove_const<Box>::type box;
+    bg::assign_zero(box);
+
+    polygon_t poly;
+
+    Value val(box, boost::addressof(poly));
+
+    bgi::rtree<Value, Params> rt;
+    rt.insert(val);
+    rt.insert(std::make_pair(box, boost::addressof(poly)));
+
+    BOOST_CHECK( rt.size() == 2 );
+
+    BOOST_CHECK( rt.remove(val) == 1 );
+    BOOST_CHECK( rt.remove(std::make_pair(box, boost::addressof(poly))) == 1 );
+
+    BOOST_CHECK( rt.size() == 0 );
+}
+
 template <typename Params>
 void test_point()
 {
@@ -75,6 +104,10 @@ int test_main(int, char* [])
     //test_rtree< Box const, bgi::linear<16> >();
     //test_rtree< Box const, bgi::quadratic<4> >();
     //test_rtree< Box const, bgi::rstar<4> >();
+
+    test_pair_geom_ptr< Box, bgi::linear<16> >();
+    test_pair_geom_ptr< Box, bgi::quadratic<4> >();
+    test_pair_geom_ptr< Box, bgi::rstar<4> >();
 
     test_point< bgi::linear<16> >();
     test_point< bgi::quadratic<4> >();
