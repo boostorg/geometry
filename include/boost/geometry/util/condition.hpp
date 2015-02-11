cff2ee9d6c30afba4fe6cf64b3d65080372f8a5d
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2015-02-11 20:29:47 +0100

    [util] Add comments about the BOOST_GEOMETRY_CONDITION() implementation.

--- include/boost/geometry/util/condition.hpp
+++ include/boost/geometry/util/condition.hpp
@@ -15,11 +15,21 @@
 #include <boost/config.hpp>
 
 
+// The macro defined in this file allows to suppress the MSVC
+// compiler warning C4127: conditional expression is constant
+
 #ifdef BOOST_MSVC
 
+// NOTE: The code commented out below contains an alternative implementation
+// of a macro using a free function. It was left here in case if in the future
+// version of MSVC for the code currently used in the macro implementation
+// the warning was generated.
+
+//#ifndef DOXYGEN_NO_DETAIL
 //namespace boost { namespace geometry { namespace detail {
 //BOOST_FORCEINLINE bool condition(bool const b) { return b; }
 //}}} // boost::geometry::detail
+//#endif // DOXYGEN_NO_DETAIL
 //#define BOOST_GEOMETRY_CONDITION(CONDITION) boost::geometry::detail::condition(CONDITION)
 
 #define BOOST_GEOMETRY_CONDITION(CONDITION) ((void)0, (CONDITION))
