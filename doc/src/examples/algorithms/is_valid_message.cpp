7c49946b390443e018c67b92d93575480e02296b
Menelaos Karavelas <menelaos.karavelas@gmail.com>
2015-02-25 15:17:28 +0200

    [doc][examples][is_valid] fit statement into a single line

--- doc/src/examples/algorithms/is_valid_message.cpp
+++ doc/src/examples/algorithms/is_valid_message.cpp
@@ -25,9 +25,7 @@ int main()
     typedef boost::geometry::model::polygon<point_type> polygon_type;
 
     polygon_type poly;
-    boost::geometry::read_wkt(
-        "POLYGON((0 0,0 10,10 10,10 0,0 0),(0 0,9 1,9 2,0 0),(0 0,2 9,1 9,0 0),(2 9,9 2,9 9,2 9))"
-            , poly);
+    boost::geometry::read_wkt("POLYGON((0 0,0 10,10 10,10 0,0 0),(0 0,9 1,9 2,0 0),(0 0,2 9,1 9,0 0),(2 9,9 2,9 9,2 9))", poly);
 
     std::string message;
     bool valid = boost::geometry::is_valid(poly, message);
