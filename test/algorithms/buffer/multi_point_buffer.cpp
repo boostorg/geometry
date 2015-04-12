ca36bc77859865cf83948568f0e81047c9baa846
Barend Gehrels <barend@xs4all.nl>
2015-04-12 17:37:00 +0200

    [buffer][test] enable two tests which are now OK for SIDE_OF_INTERSECTION approach

--- test/algorithms/buffer/multi_point_buffer.cpp
+++ test/algorithms/buffer/multi_point_buffer.cpp
@@ -60,15 +60,14 @@ void test_all()
     {
         bg::strategy::buffer::point_square point_strategy;
 
-
         test_with_custom_strategies<multi_point_type, polygon>("grid_a50",
                 grid_a, join_miter, end_flat,
                 distance_strategy(0.5), side_strategy, point_strategy, 7.0);
 
-#if defined(BOOST_GEOMETRY_BUFFER_INCLUDE_FAILING_TESTS)
+#if defined(BOOST_GEOMETRY_BUFFER_USE_SIDE_OF_INTERSECTION)
         test_with_custom_strategies<multi_point_type, polygon>("grid_a54",
                 grid_a, join_miter, end_flat,
-                distance_strategy(0.54), side_strategy, point_strategy, 99);
+                distance_strategy(0.54), side_strategy, point_strategy, 7.819);
 #endif
 
     }
