282839501d50ce6cef1be3b5706286ca4e413585
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2015-05-01 03:05:48 +0200

    [doc] Add documentation and examples for relate() and relation() functions.

--- doc/make_qbk.py
+++ doc/make_qbk.py
@@ -86,7 +86,7 @@ algorithms = ["append", "assign", "make", "clear"
     , "envelope", "equals", "expand", "for_each", "is_simple", "is_valid"
     , "intersection", "intersects", "length", "num_geometries"
     , "num_interior_rings", "num_points", "num_segments", "overlaps"
-    , "perimeter", "reverse", "simplify", "sym_difference", "touches"
+    , "perimeter", "relate", "relation", "reverse", "simplify", "sym_difference", "touches"
     , "transform", "union", "unique", "within"]
 
 access_functions = ["get", "set", "exterior_ring", "interior_rings"
