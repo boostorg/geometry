5a0ef469814e6a01dc7d3191611cde5df6d6356d
Menelaos Karavelas <menelaos.karavelas@gmail.com>
2015-05-13 19:30:20 +0300

    [util][math] add missing inlines in r2d<>() and d2r<>()

--- include/boost/geometry/util/math.hpp
+++ include/boost/geometry/util/math.hpp
@@ -494,14 +494,14 @@ inline bool larger(T1 const& a, T2 const& b)
 
 
 template <typename T>
-T d2r()
+inline T d2r()
 {
     static T const conversion_coefficient = geometry::math::pi<T>() / T(180.0);
     return conversion_coefficient;
 }
 
 template <typename T>
-T r2d()
+inline T r2d()
 {
     static T const conversion_coefficient = T(180.0) / geometry::math::pi<T>();
     return conversion_coefficient;
