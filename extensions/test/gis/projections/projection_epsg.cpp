ce8e702cb6b3359cfa646594eeccef7c8be90fd8
barendgehrels <barend@xs4all.nl>
2015-05-27 23:27:18 +0200

    [projections] remove unused type

--- extensions/test/gis/projections/projection_epsg.cpp
+++ extensions/test/gis/projections/projection_epsg.cpp
@@ -42,9 +42,6 @@ void test_one(double lon, double lat,
               typename bg::coordinate_type<P2>::type x,
               typename bg::coordinate_type<P2>::type y)
 {
-    typedef typename bg::coordinate_type<P2>::type coord_type;
-    boost::ignore_unused<coord_type>();
-
     typedef bg::projections::epsg_traits<E, P1, P2> epsg_traits;
     bg::projections::parameters par = bg::projections::detail::pj_init_plus(epsg_traits::par());
 
