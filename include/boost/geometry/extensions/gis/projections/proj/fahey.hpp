40d22d58580e194119d717a0478cb350996b1f8a
barendgehrels <barend@xs4all.nl>
2015-05-28 23:18:53 +0200

    [projections] ad missing includes

--- include/boost/geometry/extensions/gis/projections/proj/fahey.hpp
+++ include/boost/geometry/extensions/gis/projections/proj/fahey.hpp
@@ -42,6 +42,7 @@
 #include <boost/geometry/extensions/gis/projections/impl/base_dynamic.hpp>
 #include <boost/geometry/extensions/gis/projections/impl/projects.hpp>
 #include <boost/geometry/extensions/gis/projections/impl/factory_entry.hpp>
+#include <boost/geometry/extensions/gis/projections/impl/aasincos.hpp>
 
 namespace boost { namespace geometry { namespace projections
 {
