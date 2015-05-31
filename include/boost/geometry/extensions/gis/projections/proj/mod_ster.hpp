147df5988ffefeffce063582c956552a5fdd55be
barendgehrels <barend@xs4all.nl>
2015-05-31 14:22:51 +0200

    [projections] change in generation

--- include/boost/geometry/extensions/gis/projections/proj/mod_ster.hpp
+++ include/boost/geometry/extensions/gis/projections/proj/mod_ster.hpp
@@ -18,7 +18,7 @@
 // Last updated version of proj: 4.9.1
 
 // Original copyright notice:
- 
+
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
 // to deal in the Software without restriction, including without limitation
@@ -36,7 +36,6 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
-
 
 #include <boost/math/special_functions/hypot.hpp>
 
