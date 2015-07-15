79800dd3cb56964458deb030ebf35cd3b2c08fca
Barend Gehrels <barend@xs4all.nl>
2015-07-15 13:12:15 +0200

    [union][test] adapt one of the testcases for NO_ROBUSTNESS

--- test/algorithms/set_operations/union/union_multi.cpp
+++ test/algorithms/set_operations/union/union_multi.cpp
@@ -126,7 +126,12 @@ void test_areal()
 
     test_one<Polygon, MultiPolygon, MultiPolygon>("ticket_9081",
         ticket_9081[0], ticket_9081[1],
-        4, 0, 31, 0.2187385);
+#if defined(BOOST_GEOMETRY_NO_ROBUSTNESS)
+        3,
+#else
+        4,
+#endif
+        0, 31, 0.2187385);
 }
 
 template <typename P>
