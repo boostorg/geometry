36cdd256747a9cadb28577a45d6926553aef5a5c
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2015-08-05 00:55:21 +0200

    [intersection] For Box/Box take into account machine epsilon.

--- include/boost/geometry/algorithms/detail/overlay/intersection_box_box.hpp
+++ include/boost/geometry/algorithms/detail/overlay/intersection_box_box.hpp
@@ -1,6 +1,11 @@
 // Boost.Geometry (aka GGL, Generic Geometry Library)
 
-// Copyright (c) 2007-2014 Barend Gehrels, Amsterdam, the Netherlands.
+// Copyright (c) 2007-2015 Barend Gehrels, Amsterdam, the Netherlands.
+
+// This file was modified by Oracle on 2015.
+// Modifications copyright (c) 2015, Oracle and/or its affiliates.
+
+// Contributed and/or modified by Adam Wulkiewicz, on behalf of Oracle
 
 // Use, modification and distribution is subject to the Boost Software License,
 // Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
@@ -12,7 +17,7 @@
 
 #include <boost/geometry/core/access.hpp>
 #include <boost/geometry/core/coordinate_type.hpp>
-
+#include <boost/geometry/util/math.hpp>
 
 namespace boost { namespace geometry
 {
@@ -44,13 +49,29 @@ struct intersection_box_box
         ct max1 = get<max_corner, Dimension>(box1);
         ct max2 = get<max_corner, Dimension>(box2);
 
-        if (max1 < min2 || max2 < min1)
+        if (math::smaller(max1, min2) || math::smaller(max2, min1))
         {
             return false;
         }
+        
+        ct const res_min = min1 < min2 ? min2 : min1;
+        ct const res_max = max1 > max2 ? max2 : max1;
+
         // Set dimensions of output coordinate
-        set<min_corner, Dimension>(box_out, min1 < min2 ? min2 : min1);
-        set<max_corner, Dimension>(box_out, max1 > max2 ? max2 : max1);
+        // This condition is needed because math::smaller() compares values WRT
+        // machine epsilon, so max may be lesser than min when compared with
+        // raw operator<
+        if (res_min <= res_max)
+        {
+            set<min_corner, Dimension>(box_out, res_min);
+            set<max_corner, Dimension>(box_out, res_max);
+        }
+        else
+        {
+            set<min_corner, Dimension>(box_out, res_max);
+            set<max_corner, Dimension>(box_out, res_min);
+        }
+        
 
         return intersection_box_box<Dimension + 1, DimensionCount>
                ::apply(box1, box2, robust_policy, box_out, strategy);
