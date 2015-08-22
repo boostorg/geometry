069316c591001ef686f1e941cff254bc965a3807
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2015-08-23 00:23:32 +0200

    [extensions][nsphere][index] Update index-related nsphere code accoring to the latest changes in the rtree.

--- include/boost/geometry/extensions/nsphere/nsphere.hpp
+++ include/boost/geometry/extensions/nsphere/nsphere.hpp
@@ -1,8 +1,9 @@
 // Boost.Geometry (aka GGL, Generic Geometry Library)
 
-// Copyright (c) 2007-2012 Barend Gehrels, Amsterdam, the Netherlands.
-// Copyright (c) 2008-2012 Bruno Lalande, Paris, France.
-// Copyright (c) 2009-2012 Mateusz Loskot, London, UK.
+// Copyright (c) 2007-2015 Barend Gehrels, Amsterdam, the Netherlands.
+// Copyright (c) 2008-2015 Bruno Lalande, Paris, France.
+// Copyright (c) 2009-2015 Mateusz Loskot, London, UK.
+// Copyright (c) 2011-2015 Adam Wulkiewicz, Lodz, Poland.
 
 // Parts of Boost.Geometry are redesigned from Geodan's Geographic Library
 // (geolib/GGL), copyright (c) 1995-2010 Geodan, Amsterdam, the Netherlands.
@@ -41,7 +42,8 @@
 #include <boost/geometry/extensions/nsphere/algorithms/equals.hpp>
 #include <boost/geometry/extensions/nsphere/algorithms/centroid.hpp>
 
-#include <boost/geometry/extensions/nsphere/index/indexable.hpp>
+#include <boost/geometry/extensions/nsphere/index/detail/is_bounding_geometry.hpp>
+#include <boost/geometry/extensions/nsphere/index/detail/is_indexable.hpp>
 #include <boost/geometry/extensions/nsphere/index/detail/algorithms/content.hpp>
 #include <boost/geometry/extensions/nsphere/index/detail/algorithms/is_valid.hpp>
 #include <boost/geometry/extensions/nsphere/index/detail/algorithms/margin.hpp>
