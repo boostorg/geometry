65bb0dcb41b328f6695945932cec252b0531301c
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2016-08-09 21:02:54 +0200

    [formulas] Add geo_to_cart3d() calculating north and east vectors together with converted point.

--- include/boost/geometry/formulas/geographic.hpp
+++ include/boost/geometry/formulas/geographic.hpp
@@ -44,6 +44,7 @@ static inline Point3d geo_to_cart3d(PointGeo const& point_geo, Spheroid const& s
     Point3d res;
 
     calc_t const sin_lat = sin(lat);
+
     // "unit" spheroid, a = 1
     calc_t const N = c1 / math::sqrt(c1 - e_sqr * math::sqr(sin_lat));
     calc_t const N_cos_lat = N * cos(lat);
@@ -53,6 +54,39 @@ static inline Point3d geo_to_cart3d(PointGeo const& point_geo, Spheroid const& s
     set<2>(res, N * (c1 - e_sqr) * sin_lat);
 
     return res;
+}
+
+template <typename PointGeo, typename Spheroid, typename Point3d>
+static inline void geo_to_cart3d(PointGeo const& point_geo, Point3d & result, Point3d & north, Point3d & east, Spheroid const& spheroid)
+{
+    typedef typename coordinate_type<Point3d>::type calc_t;
+
+    calc_t const c1 = 1;
+    calc_t const e_sqr = eccentricity_sqr<calc_t>(spheroid);
+
+    calc_t const lon = get_as_radian<0>(point_geo);
+    calc_t const lat = get_as_radian<1>(point_geo);
+
+    calc_t const sin_lon = sin(lon);
+    calc_t const cos_lon = cos(lon);
+    calc_t const sin_lat = sin(lat);
+    calc_t const cos_lat = cos(lat);
+
+    // "unit" spheroid, a = 1
+    calc_t const N = c1 / math::sqrt(c1 - e_sqr * math::sqr(sin_lat));
+    calc_t const N_cos_lat = N * cos_lat;
+
+    set<0>(result, N_cos_lat * cos_lon);
+    set<1>(result, N_cos_lat * sin_lon);
+    set<2>(result, N * (c1 - e_sqr) * sin_lat);
+
+    set<0>(east, -sin_lon);
+    set<1>(east, cos_lon);
+    set<2>(east, 0);
+
+    set<0>(north, -sin_lat * cos_lon);
+    set<1>(north, -sin_lat * sin_lon);
+    set<2>(north, cos_lat);
 }
 
 template <typename PointGeo, typename Point3d, typename Spheroid>
@@ -71,14 +105,16 @@ static inline PointGeo cart3d_to_geo(Point3d const& point_3d, Spheroid const& sp
     calc_t const xy_l = math::sqrt(math::sqr(x) + math::sqr(y));
 
     calc_t const lonr = atan2(y, x);
+    
+    // NOTE: Alternative version
     // http://www.iag-aig.org/attach/989c8e501d9c5b5e2736955baf2632f5/V60N2_5FT.pdf
-    //calc_t const lonr = c2 * atan2(y, x + xy_l);
+    // calc_t const lonr = c2 * atan2(y, x + xy_l);
     
     calc_t const latr = atan2(z, (c1 - e_sqr) * xy_l);
 
-    // TODO: adjust the result iteratively?
+    // NOTE: If h is equal to 0 then there is no need to improve value of latitude
+    //       because then N_i / (N_i + h_i) = 1
     // http://www.navipedia.net/index.php/Ellipsoidal_and_Cartesian_Coordinates_Conversion
-    // It looks good as it is, at least for "unit" spheroid
 
     PointGeo res;
 
