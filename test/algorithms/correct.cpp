e59931473ac8e0040a810dc3a06e70a23fe55bd7
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2017-04-20 19:02:45 +0200

    [test][correct] Add missing copyright notes.

--- test/algorithms/correct.cpp
+++ test/algorithms/correct.cpp
@@ -5,6 +5,10 @@
 // Copyright (c) 2008-2012 Bruno Lalande, Paris, France.
 // Copyright (c) 2009-2012 Mateusz Loskot, London, UK.
 
+// This file was modified by Oracle on 2017.
+// Modifications copyright (c) 2017 Oracle and/or its affiliates.
+// Contributed and/or modified by Adam Wulkiewicz, on behalf of Oracle
+
 // Parts of Boost.Geometry are redesigned from Geodan's Geographic Library
 // (geolib/GGL), copyright (c) 1995-2010 Geodan, Amsterdam, the Netherlands.
 
