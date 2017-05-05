d133efe07122aaa9bb79ca86adf897de58f88dad
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2017-05-05 03:46:55 +0200

    [srs] Move projections from extensions to main part of the library.
    Move files from geometry/extensions/gis/projections to geometry/srs and
    geometry/srs/projections.
    Move project_transformer to strategies/transform.
    
    Change namespace of bg::projection<> to bg::srs::projection<>.
    
    Move dms_parser from extensions/gis/geographic/strategies to
    srs/projections/impl and from strategy::parse namespace to
    projections::detail namespace.

---
+++ include/boost/geometry/srs/parameters.hpp
@@ -0,0 +1,95 @@
+// Boost.Geometry (aka GGL, Generic Geometry Library)
+
+// Copyright (c) 2008-2012 Barend Gehrels, Amsterdam, the Netherlands.
+
+// This file was modified by Oracle on 2017.
+// Modifications copyright (c) 2017, Oracle and/or its affiliates.
+// Contributed and/or modified by Adam Wulkiewicz, on behalf of Oracle
+
+// Use, modification and distribution is subject to the Boost Software License,
+// Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef BOOST_GEOMETRY_SRS_PARAMETERS_HPP
+#define BOOST_GEOMETRY_SRS_PARAMETERS_HPP
+
+
+#include <string>
+
+#include <boost/geometry/srs/spheroid.hpp>
+
+
+namespace boost { namespace geometry { namespace srs
+{
+
+struct dynamic {};
+
+
+struct proj4
+{
+    explicit proj4(const char* s)
+        : str(s)
+    {}
+
+    explicit proj4(std::string const& s)
+        : str(s)
+    {}
+
+    std::string str;
+};
+
+
+struct epsg
+{
+    explicit epsg(int c)
+        : code(c)
+    {}
+
+    int code;
+};
+
+
+template <typename Proj, typename Model = srs::spheroid<double> >
+struct static_proj4
+{
+    typedef Proj proj_type;
+    typedef Model model_type;
+
+    static_proj4()
+    {}
+
+    explicit static_proj4(Model const& m)
+        : model(m)
+    {}
+
+    explicit static_proj4(std::string const& s)
+        : str(s)
+    {}
+
+    explicit static_proj4(const char* s)
+        : str(s)
+    {}
+
+    static_proj4(Model const& m, std::string const& s)
+        : model(m), str(s)
+    {}
+
+    static_proj4(Model const& m, const char* s)
+        : model(m), str(s)
+    {}
+
+    Model model;
+    std::string str;
+};
+
+
+template <int Code>
+struct static_epsg
+{
+    static const int code = Code;
+};
+
+
+}}} // namespace boost::geometry::srs
+
+#endif // BOOST_GEOMETRY_SRS_PARAMETERS_HPP
