c18abb79a7545bedb560627b2c457cd14c618e2e
Rylynnn <jingry0321@163.com>
2017-07-31 14:01:59 +0800

    Improve flat_approximation.hpp

--- include/boost/geometry/formulas/flat_approximation.hpp
+++ include/boost/geometry/formulas/flat_approximation.hpp
@@ -44,46 +44,16 @@ public:
     {
         result_type result;
         
-        CT const c2 = CT(2);
-        CT const earth_r = CT(6317.0);
-        CT const earth_f = formula::flattening<CT>(spheriod);
-        CT const earth_e2 = earth_f * (c2 - earth_f); 
-
         CT const lon1 = bg::get_as_radian<0>(p1);
         CT const lat1 = bg::get_as_radian<1>(p1);
         CT const lon2 = bg::get_as_radian<0>(p2);    
         CT const lat2 = bg::get_as_radian<1>(p2);    
         CT const lon4 = bg::get_as_radian<0>(p4);
         CT const lat4 = bg::get_as_radian<1>(p4);
-        
-        CT dlat = lat2 - lat1;
-        CT dlon = lon2 - lon1;
-        
-        CT R1 = earth_r * (1 - earth_e2) /
-                      bg::math::sqrt((1 - earth_e2 * bg::math::sqr(sin(lat1)))
-                              * (1 - earth_e2 * bg::math::sqr(sin(lat1)))
-                              * (1 - earth_e2 * bg::math::sqr(sin(lat1))));
-        CT R2 = earth_r / bg::math::sqrt(1 - earth_e2 * bg::math::sqr(sin(lat1)));
-        
-        CT dis_North = R1 * dlat;
-        CT dis_East = R2 * cos(lat1) * dlon;
-    
-        CT const distance_result1 = bg::math::sqrt(bg::math::sqr(dis_North) + bg::math::sqr(dis_East));
-        
-        dlat = lat4 - lat1;
-        dlon = lon4 - lon1;
        
-        R1 = earth_r * (1 - earth_e2) / 
-             bg::math::sqrt((1 - earth_e2 * bg::math::sqr(sin(lat1)))
-                     * (1 - earth_e2 * bg::math::sqr(sin(lat1)))
-                     * (1 - earth_e2 * bg::math::sqr(sin(lat1))));
-        R2 = earth_r / bg::math::sqrt(1 - earth_e2 * bg::math::sqr(sin(lat1)));
-       
-        dis_North = R1 * dlat;
-        dis_East = R2 * cos(lat1) * dlon;
-        
-        CT const distance_result2 = bg::math::sqrt(bg::math::sqr(dis_North) + bg::math::sqr(dis_East));
-   
+        CT const distance_result1 = flat_distance(lon1, lat1, lon2, lat2, spheriod);
+        CT const distance_result2 = flat_distance(lon1, lat1, lon4, lat4, spheriod);
+
         CT const sub = distance_result1 - distance_result2;
     
         if (sub < -BOOST_GEOMETRY_EPS)
@@ -100,6 +70,35 @@ public:
         }  
         return result; 
     }
+private:
+    static inline CT flat_distance(CT const& lo1,
+                                   CT const& la1,
+                                   CT const& lo2,
+                                   CT const& la2,
+                                   Spheriod const& spher)
+    {
+        CT const c2 = CT(2);
+        CT const earth_r = spher.get_radius(); 
+        CT const earth_f = formula::flattening<CT>(spher);
+        CT const earth_e2 = earth_f * (c2 - earth_f); 
+        
+        CT const dlat = la2 - la1;
+        CT const dlon = lo2 - lo1;
+        
+        CT const R1 = earth_r * (1 - earth_e2) /
+                      bg::math::sqrt((1 - earth_e2 * bg::math::sqr(sin(la1)))
+                              * (1 - earth_e2 * bg::math::sqr(sin(la1)))
+                              * (1 - earth_e2 * bg::math::sqr(sin(la1))));
+        CT const R2 = earth_r / bg::math::sqrt(1 - earth_e2 * bg::math::sqr(sin(la1)));
+        
+        CT const dis_North = R1 * dlat;
+        CT const dis_East = R2 * cos(la1) * dlon;
+    
+        CT const distance_result = bg::math::sqrt(bg::math::sqr(dis_North) 
+                                 + bg::math::sqr(dis_East));
+        return distance_result;
+    }
+    
 };
 
 }}} // namespace boost::geometry::formula
