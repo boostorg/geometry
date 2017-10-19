2e4dc6d3051cf5d54220e0a0eb2c246b549cddd9
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2017-10-19 22:01:17 +0200

    [test][srs] Test inverse transformation and add case.

--- test/srs/transformation_interface.cpp
+++ test/srs/transformation_interface.cpp
@@ -117,7 +117,8 @@ void test_geometries()
 
 template <typename P1, typename P2, typename Tr>
 inline void test_combination(Tr const& tr, P1 const& pt,
-                             std::string const& expected)
+                             std::string const& expected_fwd,
+                             P1 const& expected_inv)
 {
     using namespace boost::geometry;
 
@@ -125,13 +126,20 @@ inline void test_combination(Tr const& tr, P1 const& pt,
 
     tr.forward(pt, pt2);
 
-    test::check_geometry(pt2, expected, 0.001);
+    test::check_geometry(pt2, expected_fwd, 0.001);
+
+    P1 pt1;
+
+    tr.inverse(pt2, pt1);
+
+    test::check_geometry(pt1, expected_inv, 0.001);
 }
 
 void test_combinations(std::string const& from, std::string const& to,
                        std::string const& in_deg,
                        std::string const& expected_deg,
-                       std::string const& expected_rad)
+                       std::string const& expected_rad,
+                       std::string const& expected_inv_deg)
 {
     using namespace boost::geometry;
     using namespace boost::geometry::model;
@@ -151,15 +159,20 @@ void test_combinations(std::string const& from, std::string const& to,
     ll_r r(bg::get_as_radian<0>(d), bg::get_as_radian<1>(d));
     xy c(bg::get<0>(r), bg::get<1>(r));
 
-    test_combination<xy, xy>(tr, c, expected_rad);
-    test_combination<xy, ll_r>(tr, c, expected_rad);
-    test_combination<xy, ll_d>(tr, c, expected_deg);
-    test_combination<ll_r, xy>(tr, r, expected_rad);
-    test_combination<ll_r, ll_r>(tr, r, expected_rad);
-    test_combination<ll_r, ll_d>(tr, r, expected_deg);
-    test_combination<ll_d, xy>(tr, d, expected_rad);
-    test_combination<ll_d, ll_r>(tr, d, expected_rad);
-    test_combination<ll_d, ll_d>(tr, d, expected_deg);
+    ll_d inv_d;
+    bg::read_wkt(expected_inv_deg, inv_d);
+    ll_r inv_r(bg::get_as_radian<0>(inv_d), bg::get_as_radian<1>(inv_d));
+    xy inv_c(bg::get<0>(inv_r), bg::get<1>(inv_r));
+
+    test_combination<xy, xy>(tr, c, expected_rad, inv_c);
+    test_combination<xy, ll_r>(tr, c, expected_rad, inv_c);
+    test_combination<xy, ll_d>(tr, c, expected_deg, inv_c);
+    test_combination<ll_r, xy>(tr, r, expected_rad, inv_r);
+    test_combination<ll_r, ll_r>(tr, r, expected_rad, inv_r);
+    test_combination<ll_r, ll_d>(tr, r, expected_deg, inv_r);
+    test_combination<ll_d, xy>(tr, d, expected_rad, inv_d);
+    test_combination<ll_d, ll_r>(tr, d, expected_rad, inv_d);
+    test_combination<ll_d, ll_d>(tr, d, expected_deg, inv_d);
 }
 
 int test_main(int, char*[])
@@ -171,12 +184,21 @@ int test_main(int, char*[])
                       "+proj=longlat +ellps=airy +datum=OSGB36 +no_defs",
                       "POINT(18.5 54.2)",
                       "POINT(18.5038403269 54.1993274575)",
-                      "POINT(0.322952937968 0.9459567165)");
+                      "POINT(0.322952937968 0.9459567165)",
+                      "POINT(18.5 54.2)");
     test_combinations("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs",
                       "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +no_defs",
                       "POINT(18.5 54.2)",
                       "POINT(2059410.57968 7208125.2609)",
-                      "POINT(2059410.57968 7208125.2609)");
-
+                      "POINT(2059410.57968 7208125.2609)",
+                      "POINT(18.5 54.2)");
+    test_combinations("+proj=longlat +ellps=clrk80 +units=m +no_defs",
+                      "+proj=tmerc +lat_0=0 +lon_0=-62 +k=0.9995000000000001 +x_0=400000 +y_0=0 +ellps=clrk80 +units=m +no_defs",
+                      "POINT(1 1)",
+                      "POINT(9413505.3284665551 237337.74515944949)",
+                      "POINT(9413505.3284665551 237337.74515944949)",
+                      // this result seems to be wrong, it's the same with projection
+                      "POINT(-2.4463131191981073 1.5066638962045082)");
+    
     return 0;
 }
