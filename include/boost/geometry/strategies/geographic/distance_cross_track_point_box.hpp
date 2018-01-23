719fe851bf64388784c8abd119756247372b177e
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2018-01-23 21:12:32 +0100

    [strategy] Fix strategy passing in helper function of geographic distance P/B strategy.

--- include/boost/geometry/strategies/geographic/distance_cross_track_point_box.hpp
+++ include/boost/geometry/strategies/geographic/distance_cross_track_point_box.hpp
@@ -1,8 +1,9 @@
 // Boost.Geometry (aka GGL, Generic Geometry Library)
 
-// Copyright (c) 2017, Oracle and/or its affiliates.
+// Copyright (c) 2017-2018, Oracle and/or its affiliates.
 
 // Contributed and/or modified by Vissarion Fysikopoulos, on behalf of Oracle
+// Contributed and/or modified by Adam Wulkiewicz, on behalf of Oracle.
 
 // Use, modification and distribution is subject to the Boost Software License,
 // Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
@@ -174,12 +175,10 @@ public:
     static inline return_type apply(this_strategy const& strategy,
                                     T const& distance)
     {
-        Strategy s();
-
         result_from_distance
             <
                 Strategy, P, typename point_type<Box>::type
-            >::apply(s, distance);
+            >::apply(strategy, distance);
     }
 };
 
