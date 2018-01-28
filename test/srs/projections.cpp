350f4990b3cac45dc9a5188f9a864dd64230a815
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2018-01-28 03:30:44 +0100

    [test][srs] Fix unused parameter warning.

--- test/srs/projections.cpp
+++ test/srs/projections.cpp
@@ -5,6 +5,10 @@
 // Copyright (c) 2008-2012 Bruno Lalande, Paris, France.
 // Copyright (c) 2009-2012 Mateusz Loskot, London, UK.
 
+// This file was modified by Oracle on 2018.
+// Modifications copyright (c) 2018, Oracle and/or its affiliates.
+// Contributed and/or modified by Adam Wulkiewicz, on behalf of Oracle
+
 // Parts of Boost.Geometry are redesigned from Geodan's Geographic Library
 // (geolib/GGL), copyright (c) 1995-2010 Geodan, Amsterdam, the Netherlands.
 
@@ -36,7 +40,7 @@
 
 namespace srs = bg::srs;
 
-inline void check(double v, double ve, std::string const& name, std::string const& axis)
+inline void check(double v, double ve, std::string const& /*name*/, std::string const& axis)
 {
     // (non-existing) BOOST_CHECK_CLOSE_MESSAGE(v, ve, 0.001, "\n" << name << " " << axis << " -> " << v << " != " << ve);
     BOOST_CHECK_CLOSE(v, ve, 0.001);
