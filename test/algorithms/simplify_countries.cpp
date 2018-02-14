3335bf970c6db030596d1a133dda4f76f9de8ceb
Barend Gehrels <barend@xs4all.nl>
2018-02-14 15:01:33 +0100

    [test] update test expectations

--- test/algorithms/simplify_countries.cpp
+++ test/algorithms/simplify_countries.cpp
@@ -124,49 +124,49 @@ void test_all()
     // Area ratio (first) can increase or decrease
     // Perimeter ratio (second) should decrease.
     test_one<mp>("gr", gr, 100, 0.999893, 0.999761);
-    test_one<mp>("gr", gr, 200, 0.999727, 0.99889);
-    test_one<mp>("gr", gr, 500, 0.999106, 0.995726);
-    test_one<mp>("gr", gr, 1000, 0.998011, 0.991557);
-    test_one<mp>("gr", gr, 2000, 0.994586, 0.977572);
-    test_one<mp>("gr", gr, 5000, 0.980025, 0.91269);
-    test_one<mp>("gr", gr, 10000, 0.971832, 0.786512);
-    test_one<mp>("gr", gr, 20000, 0.947049, 0.600652); // many islands disappeared
+    test_one<mp>("gr", gr, 200, 0.999731, 0.998894);
+    test_one<mp>("gr", gr, 500, 0.998969, 0.995778);
+    test_one<mp>("gr", gr, 1000, 0.99721, 0.990555);
+    test_one<mp>("gr", gr, 2000, 0.993177, 0.975566);
+    test_one<mp>("gr", gr, 5000, 0.976362, 0.903488);
+    test_one<mp>("gr", gr, 10000, 0.954145, 0.770951);
+    test_one<mp>("gr", gr, 20000, 0.94543, 0.611661); // many islands disappeared
 
     test_one<mp>("it", it, 100, 1.00001, 0.999815);
-    test_one<mp>("it", it, 200, 1.00009, 0.9991);
-    test_one<mp>("it", it, 500, 1.00021, 0.996903);
-    test_one<mp>("it", it, 1000, 1.00059, 0.993098);
-    test_one<mp>("it", it, 2000, 1.001, 0.985255);
-    test_one<mp>("it", it, 5000, 1.00212, 0.929793);
-    test_one<mp>("it", it, 10000, 1.01076, 0.875699);
-    test_one<mp>("it", it, 20000, 1.01042, 0.829402);
+    test_one<mp>("it", it, 200, 1.00009, 0.999099);
+    test_one<mp>("it", it, 500, 1.00021, 0.996854);
+    test_one<mp>("it", it, 1000, 1.00049, 0.992692);
+    test_one<mp>("it", it, 2000, 1.0009, 0.983258);
+    test_one<mp>("it", it, 5000, 1.00185, 0.932119);
+    test_one<mp>("it", it, 10000, 1.01091, 0.869922);
+    test_one<mp>("it", it, 20000, 1.00959, 0.8281);
 
     test_one<mp>("nl", nl, 100, 0.999894, 0.999817);
-    test_one<mp>("nl", nl, 200, 0.999635, 0.99909);
-    test_one<mp>("nl", nl, 500, 0.999015, 0.996253);
-    test_one<mp>("nl", nl, 1000, 0.997958, 0.991893);
-    test_one<mp>("nl", nl, 2000, 0.996671, 0.982696);
-    test_one<mp>("nl", nl, 5000, 0.992621, 0.934318);
-    test_one<mp>("nl", nl, 10000, 0.973356, 0.807464);
-    test_one<mp>("nl", nl, 20000, 0.983277, 0.742468);
+    test_one<mp>("nl", nl, 200, 0.999641, 0.999096);
+    test_one<mp>("nl", nl, 500, 0.999115, 0.996286);
+    test_one<mp>("nl", nl, 1000, 0.997689, 0.991921);
+    test_one<mp>("nl", nl, 2000, 0.995369, 0.982222);
+    test_one<mp>("nl", nl, 5000, 0.991539, 0.9332);
+    test_one<mp>("nl", nl, 10000, 0.974408, 0.806818);
+    test_one<mp>("nl", nl, 20000, 0.979702, 0.752186);
 
     test_one<mp>("no", no, 100, 0.999967, 0.999976);
-    test_one<mp>("no", no, 200, 0.999821, 0.999731);
-    test_one<mp>("no", no, 500, 0.999314, 0.998092);
-    test_one<mp>("no", no, 1000, 0.998456, 0.994053);
-    test_one<mp>("no", no, 2000, 0.996493, 0.985213);
-    test_one<mp>("no", no, 5000, 0.99086, 0.965176);
-    test_one<mp>("no", no, 10000, 0.976511, 0.909933);
-    test_one<mp>("no", no, 20000, 0.9549, 0.785917);
+    test_one<mp>("no", no, 200, 0.999821, 0.999732);
+    test_one<mp>("no", no, 500, 0.999319, 0.998098);
+    test_one<mp>("no", no, 1000, 0.998455, 0.99401);
+    test_one<mp>("no", no, 2000, 0.9963, 0.984416);
+    test_one<mp>("no", no, 5000, 0.988401, 0.957444);
+    test_one<mp>("no", no, 10000, 0.974084, 0.90103);
+    test_one<mp>("no", no, 20000, 0.953065, 0.784675);
 
-    test_one<mp>("uk", uk, 100, 0.999948, 0.99988);
-    test_one<mp>("uk", uk, 200, 0.999853, 0.999284);
-    test_one<mp>("uk", uk, 500, 0.999551, 0.996887);
-    test_one<mp>("uk", uk, 1000, 0.999096, 0.992345);
-    test_one<mp>("uk", uk, 2000, 0.998076, 0.984032);
-    test_one<mp>("uk", uk, 5000, 0.991446, 0.943896);
-    test_one<mp>("uk", uk, 10000, 0.989576, 0.870631);
-    test_one<mp>("uk", uk, 20000, 0.977384, 0.738103);
+    test_one<mp>("uk", uk, 100, 0.999949, 0.99988);
+    test_one<mp>("uk", uk, 200, 0.999854, 0.999289);
+    test_one<mp>("uk", uk, 500, 0.999557, 0.996874);
+    test_one<mp>("uk", uk, 1000, 0.998942, 0.992074);
+    test_one<mp>("uk", uk, 2000, 0.997703, 0.983059);
+    test_one<mp>("uk", uk, 5000, 0.990991, 0.942994);
+    test_one<mp>("uk", uk, 10000, 0.988484, 0.871371);
+    test_one<mp>("uk", uk, 20000, 0.972889, 0.736474);
 }
 
 int test_main(int, char* [])
