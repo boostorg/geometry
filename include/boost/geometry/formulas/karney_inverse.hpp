485a2f9a0f6d245ae3b2fb3762e664ced7754ea9
Adeel Ahmad <adeelahmad14@hotmail.com>
2018-06-11 18:44:27 +0500

    [formulas] Find starting point for inverse problem (short lines)

--- include/boost/geometry/formulas/karney_inverse.hpp
+++ include/boost/geometry/formulas/karney_inverse.hpp
@@ -50,11 +50,17 @@ class karney_inverse
 public:
     typedef result_inverse<CT> result_type;
     static CT constexpr c0 = 0;
+    static CT constexpr c0_001 = 0.001;
+    static CT constexpr c0_01 = 0.01;
+    static CT constexpr c0_1 = 0.1;
+    static CT constexpr c0_5 = 0.5;
     static CT constexpr c1 = 1;
     static CT constexpr c2 = 2;
     static CT constexpr c3 = 3;
-    static CT constexpr c180 = 180;
+    static CT constexpr c6 = 6;
     static CT constexpr c90 = 90;
+    static CT constexpr c180 = 180;
+    static CT constexpr c200 = 200;
 
     template <typename T1, typename T2, typename Spheroid>
     static inline result_type apply(T1 const& lo1,
@@ -71,13 +77,19 @@ public:
         CT lon1 = lo1;
         CT lon2 = lo2;
 
-        CT tiny = std::sqrt(std::numeric_limits<CT>::min());
-
         CT const a = CT(get_radius<0>(spheroid));
         CT const b = CT(get_radius<2>(spheroid));
         CT const f = formula::flattening<CT>(spheroid);
         CT const one_minus_f = c1 - f;
         CT const two_minus_f = c2 - f;
+
+        CT const tol0 = std::numeric_limits<CT>::epsilon();
+        CT const tol1 = c200 * tol0;
+        CT const tol2 = sqrt(tol0);
+        CT const etol2 = c0_1 * tol2 /
+            sqrt(std::max(c0_001, std::abs(f)) * std::min(c1, c1 - f / c2) / c2);
+
+        CT tiny = std::sqrt(std::numeric_limits<CT>::min());
 
         CT const n = f / two_minus_f;
         CT const e2 = f * two_minus_f;
@@ -198,10 +210,12 @@ public:
             CT sigma12 = std::atan2(std::max(c0, cos_sigma1 * sin_sigma2 - sin_sigma1 * cos_sigma2),
                                              cos_sigma1 * cos_sigma2 + sin_sigma1 * sin_sigma2);
 
+            CT dummy;
             meridian_length(n, ep2, sigma12, sin_sigma1, cos_sigma1, dn1,
                                              sin_sigma2, cos_sigma2, dn2,
                                              cos_beta1, cos_beta2, s12x,
-                                             m12x, result.geodesic_scale, M21);
+                                             m12x, dummy, result.geodesic_scale,
+                                             M21, coeffs_C1);
 
 
             if (sigma12 < c1 || m12x >= c0)
@@ -227,7 +241,6 @@ public:
         if (!meridian && sin_beta1 == 0 &&
             (f <= 0 || lon12_error >= f * c180))
         {
-            std::cout << "Points lie on the equator." << std::endl;
             // Points lie on the equator.
             cos_alpha1 = cos_alpha2 = c0;
             sin_alpha1 = sin_alpha2 = c1;
@@ -239,9 +252,23 @@ public:
             if (BOOST_GEOMETRY_CONDITION(EnableGeodesicScale))
             {
                 result.geodesic_scale = cos(sigma12);
-                M21 = cos(sigma12);
             }
             a12 = lon12 / one_minus_f;
+        }
+
+        else if (!meridian)
+        {
+            // If point1 and point2 belong within a hemisphere bounded by a
+            // meridian and geodesic is neither meridional nor equatorial.
+
+            // Find the starting point for Newton's method.
+            CT dnm;
+            sigma12 = newton_start(sin_beta1, cos_beta1, dn1,
+                                 sin_beta2, cos_beta2, dn2,
+                                 lam12, sin_lam12, cos_lam12,
+                                 sin_alpha1, cos_alpha1,
+                                 sin_alpha2, cos_alpha2,
+                                 dnm, coeffs_C1, ep2, etol2, n, f);
         }
     }
 
@@ -249,14 +276,13 @@ public:
                                        CT sin_sigma1, CT cos_sigma1, CT dn1,
                                        CT sin_sigma2, CT cos_sigma2, CT dn2,
                                        CT cos_beta1, CT cos_beta2,
-                                       CT& s12x, CT& m12x,
-                                       CT& M12, CT& M21)
+                                       CT& s12x, CT& m12x, CT& m0,
+                                       CT& M12, CT& M21, CT coeffs_C1[])
     {
         CT A12x = 0, J12 = 0;
         CT expansion_A1, expansion_A2;
 
-        // Index zero element of coeffs_C1 and coeffs_C2 is unused.
-        CT coeffs_C1[SeriesOrder + 1];
+        // Index zero element of coeffs_C2 is unused.
         CT coeffs_C2[SeriesOrder + 1];
 
         if (BOOST_GEOMETRY_CONDITION(EnableDistance) ||
@@ -331,6 +357,8 @@ public:
 
         if (BOOST_GEOMETRY_CONDITION(EnableReducedLength))
         {
+            m0 = A12x;
+
             m12x = dn2 * (cos_sigma1 * sin_sigma2) -
                    dn1 * (sin_sigma1 * cos_sigma2) -
                    cos_sigma1 * cos_sigma2 * J12;
@@ -347,6 +375,124 @@ public:
         }
     }
 
+    /*
+     Return a starting point for Newton's method in sin_alpha1 and
+     cos_alpha1 (function value is -1). If Newton's method
+     doesn't need to be used, return also sin_alpha2 and
+     cos_alpha2 and function value is sig12.
+    */
+    static inline CT newton_start(CT sin_beta1, CT cos_beta1, CT dn1,
+                                  CT sin_beta2, CT cos_beta2, CT dn2,
+                                  CT lam12, CT sin_lam12, CT cos_lam12,
+                                  CT& sin_alpha1, CT& cos_alpha1,
+                                  CT& sin_alpha2, CT& cos_alpha2,
+                                  CT& dnm, CT coeffs_C1[], CT ep2,
+                                  CT etol2, CT n, CT f)
+    {
+        CT const one_minus_f = c1 - f;
+        CT sig12 = -c1;
+
+        CT sin_beta12 = sin_beta2 * cos_beta1 - cos_beta2 * sin_beta1;
+        CT cos_beta12 = cos_beta2 * cos_beta1 + sin_beta2 * sin_beta1;
+
+        CT sin_beta12a = sin_beta2 * cos_beta1 + cos_beta2 * sin_beta1;
+
+        bool shortline = cos_beta12 >= c0 && sin_beta12 < c0_5 &&
+                         cos_beta2 * lam12 < c0_5;
+
+        CT sin_omega12, cos_omega12;
+
+        if (shortline)
+        {
+            CT sin_beta_m2 = math::sqr(sin_beta1 + sin_beta2);
+
+            sin_beta_m2 /= sin_beta_m2 + math::sqr(cos_beta1 + cos_beta2);
+            dnm = math::sqrt(c1 + ep2 * sin_beta_m2);
+
+            CT omega12 = lam12 / (one_minus_f * dnm);
+
+            sin_omega12 = sin(omega12);
+            cos_omega12 = cos(omega12);
+        }
+        else
+        {
+            sin_omega12 = sin_lam12;
+            cos_omega12 = cos_lam12;
+        }
+
+        sin_alpha1 = cos_beta2 * sin_omega12;
+        // TODO: adl1995 - Resolve inaccuracy with
+        // cos_alpha1 calculation.
+        cos_alpha1 = cos_omega12 >= c0 ?
+            sin_beta12 + cos_beta2 * sin_beta1 * math::sqr(sin_omega12) / (c1 + cos_omega12) :
+            sin_beta12a - cos_beta2 * sin_beta1 * math::sqr(sin_omega12) / (c1 - cos_omega12);
+
+        CT sin_sigma12 = boost::math::hypot(sin_alpha1, cos_alpha1);
+        CT cos_sigma12 = sin_beta1 * sin_beta2 + cos_beta1 * cos_beta2 * cos_omega12;
+
+        if (shortline && sin_sigma12 < etol2)
+        {
+            sin_alpha2 = cos_beta1 * sin_omega12;
+            cos_alpha2 = sin_beta12 - cos_beta1 * sin_beta2 *
+                (cos_omega12 >= c0 ? math::sqr(sin_omega12) /
+                (c1 + cos_omega12) : c1 - cos_omega12);
+        }
+        // Skip astroid calculation if too eccentric.
+        else if (std::abs(n) > c0_1 ||
+                 cos_sigma12 >= c0 ||
+                 sin_sigma12 >= c6 * std::abs(n) * math::pi<CT>() *
+                 math::sqr(cos_beta1))
+        {
+            // Nothing to do (?).
+        }
+        else
+        {
+            // Scale lam12 and beta2 to x, y coordinate system where antipodal
+            // point is at origin and singular point is at y = 0, x = -1.
+            CT lambda_scale, beta_scale;
+
+            CT y;
+            volatile CT x;
+
+            CT lam12x = atan2(-sin_lam12, -cos_lam12);
+            if (f >= 0)
+            {
+                CT k2 = math::sqr(sin_beta1) * ep2;
+                CT epsilon = k2 / (c2 * (c1 * sqrt(c1 + k2)) + k2);
+
+                CT coeffs_A3[SeriesOrder];
+                series_expansion::evaluate_coeffs_A3<double, SeriesOrder>(n, coeffs_A3);
+
+                CT const A3 = math::horner_evaluate(epsilon, coeffs_A3, coeffs_A3 + SeriesOrder);
+
+                lambda_scale = f * cos_beta1 * A3 * math::pi<CT>();
+
+                beta_scale = lambda_scale * cos_beta1;
+
+                x = lam12x / lambda_scale;
+                y = sin_beta12a / beta_scale;
+            }
+            else
+            {
+                CT cos_beta12a = cos_beta2 * cos_beta1 - sin_beta2 * sin_beta1;
+                CT beta12a = atan2(sin_beta12a, cos_beta12a);
+
+                CT m12b, m0, dummy;
+                meridian_length(n, ep2, math::pi<CT>() + beta12a,
+                                sin_beta1, -cos_beta1, dn1,
+                                sin_beta2, cos_beta2, dn2,
+                                cos_beta1, cos_beta2, dummy,
+                                m12b, m0, dummy, dummy, coeffs_C1);
+
+                x = -c1 + m12b / (cos_beta1 * cos_beta2 * m0 * math::pi<CT>());
+                beta_scale = x < -c0_01 ? sin_beta12a / x :
+                    -f * math::sqr(cos_beta1) * math::pi<CT>();
+                lambda_scale = beta_scale / cos_beta1;
+
+                y = lam12x / lambda_scale;
+            }
+        }
+    }
 };
 
 }}} // namespace boost::geometry::formula
