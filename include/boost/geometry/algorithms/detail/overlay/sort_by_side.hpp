boostorg/geometry

commit 0d78de59749993f69d5f083ff9fe326a7aaba734

Fix potential unsigned integer overflow in find_polygons_for_source
An invalid polygon will trigger overflow when `previous_rank` is `0` as `previous_rank - 1` will overflow. This can be detected by passing an invalid polygon like `[[8128,3600],[8224,3664],[8128,3600]]` into `boost::geometry::intersection` and compiling with `-fsanitize=undefined`

include/boost/geometry/algorithms/detail/overlay/sort_by_side.hpp
@@ -521,7 +521,7 @@ private :
         {
             rp& ranked = m_ranked_points[index];
 
-            if (ranked.rank != previous_rank && ! in_polygon)
+            if (previous_rank > 0 && ranked.rank != previous_rank && ! in_polygon)
             {
                 assign_ranks(last_from_rank, previous_rank - 1, 1);
                 assign_ranks(last_from_rank + 1, previous_rank, 2);

commit 50691def6bd1b957dd937a1ba85852ccbccfcb0b

[sort_by_side] Undo commit 0d78de5 which caused several regressions in all intersection code. Fix the underlying reason for it (potential overflow) in another way, by changing rank type to signed_size_type

include/boost/geometry/algorithms/detail/overlay/sort_by_side.hpp
@@ -34,6 +34,9 @@ namespace detail { namespace overlay { namespace sort_by_side
 
 enum direction_type { dir_unknown = -1, dir_from = 0, dir_to = 1 };
 
+typedef signed_size_type rank_type;
+
+
 // Point-wrapper, adding some properties
 template <typename Point>
 struct ranked_point
@@ -64,7 +67,7 @@ struct ranked_point
     {}
 
     Point point;
-    std::size_t rank;
+    rank_type rank;
     signed_size_type zone; // index of closed zone, in uu turn there would be 2 zones
     signed_size_type turn_index;
     int operation_index; // 0,1
@@ -445,7 +448,7 @@ private :
     inline std::size_t open_count(Include const& include_functor) const
     {
         std::size_t result = 0;
-        std::size_t last_rank = 0;
+        rank_type last_rank = 0;
         for (std::size_t i = 0; i < m_ranked_points.size(); i++)
         {
             rp const& ranked_point = m_ranked_points[i];
@@ -479,7 +482,7 @@ private :
         return result;
     }
 
-    void assign_ranks(std::size_t min_rank, std::size_t max_rank, int side_index)
+    void assign_ranks(rank_type min_rank, rank_type max_rank, int side_index)
     {
         for (std::size_t i = 0; i < m_ranked_points.size(); i++)
         {
@@ -512,8 +515,8 @@ private :
     {
         bool in_polygon = true; // Because start_index is "from", arrives at the turn
         rp const& start_rp = m_ranked_points[start_index];
-        std::size_t last_from_rank = start_rp.rank;
-        std::size_t previous_rank = start_rp.rank;
+        rank_type last_from_rank = start_rp.rank;
+        rank_type previous_rank = start_rp.rank;
 
         for (std::size_t index = move<Member>(the_index, start_index);
              ;
@@ -521,7 +524,7 @@ private :
         {
             rp& ranked = m_ranked_points[index];
 
-            if (previous_rank > 0 && ranked.rank != previous_rank && ! in_polygon)
+            if (ranked.rank != previous_rank && ! in_polygon)
             {
                 assign_ranks(last_from_rank, previous_rank - 1, 1);
                 assign_ranks(last_from_rank + 1, previous_rank, 2);
@@ -552,9 +555,9 @@ private :
     {
         // Find a starting point (the first rank after an outgoing rank
         // with no polygons on the left side)
-        std::size_t start_rank = m_ranked_points.size() + 1;
+        rank_type start_rank = m_ranked_points.size() + 1;
         std::size_t start_index = 0;
-        std::size_t max_rank = 0;
+        rank_type max_rank = 0;
         for (std::size_t i = 0; i < m_ranked_points.size(); i++)
         {
             rp const& ranked_point = m_ranked_points[i];
@@ -574,10 +577,10 @@ private :
         }
 
         // Assign the zones
-        std::size_t const undefined_rank = max_rank + 1;
+        rank_type const undefined_rank = max_rank + 1;
         std::size_t zone_id = 0;
-        std::size_t last_rank = 0;
-        std::size_t rank_at_next_zone = undefined_rank;
+        rank_type last_rank = 0;
+        rank_type rank_at_next_zone = undefined_rank;
         std::size_t index = start_index;
         for (std::size_t i = 0; i < m_ranked_points.size(); i++)
         {

include/boost/geometry/algorithms/detail/overlay/traversal.hpp
@@ -557,7 +557,7 @@ struct traversal
         return m_turns[rp.turn_index].operations[rp.operation_index];
     }
 
-    inline int select_turn_in_cluster_union(std::size_t selected_rank,
+    inline int select_turn_in_cluster_union(sort_by_side::rank_type selected_rank,
             typename sbs_type::rp const& ranked_point,
             signed_size_type start_turn_index, int start_op_index) const
     {
@@ -594,7 +594,7 @@ struct traversal
             ;
     }
 
-    inline signed_size_type select_rank(sbs_type const& sbs,
+    inline sort_by_side::rank_type select_rank(sbs_type const& sbs,
                                         bool skip_isolated) const
     {
         // Take the first outgoing rank corresponding to incoming region,
@@ -632,7 +632,7 @@ struct traversal
         int& op_index, sbs_type const& sbs,
         signed_size_type start_turn_index, int start_op_index) const
     {
-        std::size_t const selected_rank = select_rank(sbs, false);
+        sort_by_side::rank_type const selected_rank = select_rank(sbs, false);
 
         int best_code = 0;
         bool result = false;
@@ -665,7 +665,7 @@ struct traversal
     inline bool analyze_cluster_intersection(signed_size_type& turn_index,
                 int& op_index, sbs_type const& sbs) const
     {
-        std::size_t const selected_rank = select_rank(sbs, true);
+        sort_by_side::rank_type const selected_rank = select_rank(sbs, true);
 
         if (selected_rank > 0)
         {