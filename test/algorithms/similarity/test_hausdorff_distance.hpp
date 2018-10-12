e70603926dbc5a0ae78e7c9c8e13e9a75a60b235
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2018-10-12 22:11:56 +0200

    [test][similarity] Fix conversion warning and formatting in discrete_hausdorff_distance test.

--- test/algorithms/similarity/test_hausdorff_distance.hpp
+++ test/algorithms/similarity/test_hausdorff_distance.hpp
@@ -1,8 +1,12 @@
 // Boost.Geometry
 
 // Copyright (c) 2018 Yaghyavardhan Singh Khangarot, Hyderabad, India.
+// Contributed and/or modified by Yaghyavardhan Singh Khangarot,
+//   as part of Google Summer of Code 2018 program.
 
-// Contributed and/or modified by Yaghyavardhan Singh Khangarot, as part of Google Summer of Code 2018 program.
+// This file was modified by Oracle on 2018.
+// Modifications copyright (c) 2018, Oracle and/or its affiliates.
+// Contributed and/or modified by Adam Wulkiewicz, on behalf of Oracle
 
 // Use, modification and distribution is subject to the Boost Software License,
 // Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
@@ -17,21 +21,17 @@
 #include <boost/geometry/strategies/strategies.hpp>
 #include <boost/variant/variant.hpp>
 
-template <typename Geometry1,typename Geometry2>
-void test_hausdorff_distance(Geometry1 const& geometry1,Geometry2 const& geometry2,
-    typename bg::distance_result
+template <typename Geometry1, typename Geometry2, typename Expected>
+void test_hausdorff_distance(Geometry1 const& geometry1, Geometry2 const& geometry2,
+                             Expected const& expected_hausdorff_distance)
+{
+    typedef typename bg::distance_result
         <
             typename bg::point_type<Geometry1>::type,
             typename bg::point_type<Geometry2>::type
-        >::type expected_hausdorff_distance )
-{
-    using namespace bg;
-    typedef typename distance_result
-        <
-            typename point_type<Geometry1>::type,
-            typename point_type<Geometry2>::type
         >::type result_type;
-    result_type h_distance = bg::discrete_hausdorff_distance(geometry1,geometry2);
+
+    result_type h_distance = bg::discrete_hausdorff_distance(geometry1, geometry2);
 
 #ifdef BOOST_GEOMETRY_TEST_DEBUG
     std::ostringstream out;
@@ -41,51 +41,46 @@ void test_hausdorff_distance(Geometry1 const& geometry1,Geometry2 const& geometr
         << std::endl
         << typeid(h_distance).name()
         << std::endl
-        << "hausdorff_distance : " << bg::discrete_hausdorff_distance(geometry1,geometry2)
+        << "hausdorff_distance : " << h_distance
         << std::endl;
     std::cout << out.str();
 #endif
 
-    BOOST_CHECK_CLOSE(h_distance, expected_hausdorff_distance, 0.01);
+    BOOST_CHECK_CLOSE(h_distance, result_type(expected_hausdorff_distance), 0.01);
 }
 
 
 
-template <typename Geometry1,typename Geometry2>
-void test_geometry(std::string const& wkt1,std::string const& wkt2,
-    typename bg::distance_result
-        <
-            typename bg::point_type<Geometry1>::type,
-            typename bg::point_type<Geometry2>::type
-        >::type expected_hausdorff_distance)
+template <typename Geometry1, typename Geometry2, typename Expected>
+void test_geometry(std::string const& wkt1, std::string const& wkt2,
+                   Expected const& expected_hausdorff_distance)
 {
     Geometry1 geometry1;
     bg::read_wkt(wkt1, geometry1);
     Geometry2 geometry2;
     bg::read_wkt(wkt2, geometry2);
-    test_hausdorff_distance(geometry1,geometry2,expected_hausdorff_distance);
+
+    test_hausdorff_distance(geometry1, geometry2, expected_hausdorff_distance);
+
 #if defined(BOOST_GEOMETRY_TEST_DEBUG)
-    test_hausdorff_distance(boost::variant<Geometry1>(geometry1),boost::variant<Geometry2>(geometry2), expected_hausdorff_distance);
+    test_hausdorff_distance(boost::variant<Geometry1>(geometry1),
+                            boost::variant<Geometry2>(geometry2),
+                            expected_hausdorff_distance);
 #endif
 }
 
-template <typename Geometry1,typename Geometry2 ,typename Strategy>
-void test_hausdorff_distance(Geometry1 const& geometry1,Geometry2 const& geometry2,Strategy strategy,
-    typename bg::distance_result
+template <typename Geometry1, typename Geometry2, typename Strategy, typename Expected>
+void test_hausdorff_distance(Geometry1 const& geometry1, Geometry2 const& geometry2,
+                             Strategy strategy, Expected const& expected_hausdorff_distance)
+{
+    typedef typename bg::distance_result
         <
             typename bg::point_type<Geometry1>::type,
             typename bg::point_type<Geometry2>::type,
             Strategy
-        >::type expected_hausdorff_distance )
-{
-    using namespace bg;
-    typedef typename distance_result
-        <
-            typename point_type<Geometry1>::type,
-            typename point_type<Geometry2>::type,
-            Strategy
         >::type result_type;
-    result_type h_distance = bg::discrete_hausdorff_distance(geometry1,geometry2,strategy);
+
+    result_type h_distance = bg::discrete_hausdorff_distance(geometry1, geometry2, strategy);
 
 #ifdef BOOST_GEOMETRY_TEST_DEBUG
     std::ostringstream out;
@@ -95,48 +90,47 @@ void test_hausdorff_distance(Geometry1 const& geometry1,Geometry2 const& geometr
         << std::endl
         << typeid(h_distance).name()
         << std::endl
-        << "hausdorff_distance : " << bg::discrete_hausdorff_distance(geometry1,geometry2,strategy)
+        << "hausdorff_distance : " << h_distance
         << std::endl;
     std::cout << out.str();
 #endif
 
-    BOOST_CHECK_CLOSE(h_distance, expected_hausdorff_distance, 0.01);
+    BOOST_CHECK_CLOSE(h_distance, result_type(expected_hausdorff_distance), 0.01);
 }
 
 
 
-template <typename Geometry1,typename Geometry2,typename Strategy>
-void test_geometry(std::string const& wkt1,std::string const& wkt2,Strategy strategy,
-    typename bg::distance_result
-        <
-            typename bg::point_type<Geometry1>::type,
-            typename bg::point_type<Geometry2>::type,
-            Strategy
-        >::type expected_hausdorff_distance)
+template <typename Geometry1, typename Geometry2, typename Strategy, typename Expected>
+void test_geometry(std::string const& wkt1, std::string const& wkt2,
+                   Strategy strategy, Expected const& expected_hausdorff_distance)
 {
     Geometry1 geometry1;
     bg::read_wkt(wkt1, geometry1);
     Geometry2 geometry2;
     bg::read_wkt(wkt2, geometry2);
-    test_hausdorff_distance(geometry1,geometry2,strategy,expected_hausdorff_distance);
+
+    test_hausdorff_distance(geometry1, geometry2, strategy, expected_hausdorff_distance);
+
 #if defined(BOOST_GEOMETRY_TEST_DEBUG)
-    test_hausdorff_distance(boost::variant<Geometry1>(geometry1),boost::variant<Geometry2>(geometry2),strategy, expected_hausdorff_distance);
+    test_hausdorff_distance(boost::variant<Geometry1>(geometry1),
+                            boost::variant<Geometry2>(geometry2),
+                            strategy, expected_hausdorff_distance);
 #endif
 }
 
 
 template <typename Geometry1,typename Geometry2>
-void test_empty_input(Geometry1 const& geometry1,Geometry2 const& geometry2)
+void test_empty_input(Geometry1 const& geometry1, Geometry2 const& geometry2)
 {
     try
     {
-        bg::discrete_hausdorff_distance(geometry1,geometry2);
+        bg::discrete_hausdorff_distance(geometry1, geometry2);
     }
     catch(bg::empty_input_exception const& )
     {
         return;
     }
-    BOOST_CHECK_MESSAGE(false, "A empty_input_exception should have been thrown" );
+    BOOST_CHECK_MESSAGE(false, "A empty_input_exception should have been thrown");
 }
 
 
