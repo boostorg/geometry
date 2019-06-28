03fdd412b04c730b685b386643d395e712141422
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2019-06-28 21:43:03 +0200

    [strategies] Add envelope_box_strategy_type to intersection strategies.

--- include/boost/geometry/strategies/geographic/intersection.hpp
+++ include/boost/geometry/strategies/geographic/intersection.hpp
@@ -197,6 +197,7 @@ struct geographic_segments
     typedef covered_by::spherical_point_box disjoint_point_box_strategy_type;
     typedef covered_by::spherical_point_box covered_by_point_box_strategy_type;
     typedef within::spherical_point_box within_point_box_strategy_type;
+    typedef envelope::spherical_box envelope_box_strategy_type;
     typedef expand::spherical_box expand_box_strategy_type;
 
     enum intersection_point_flag { ipi_inters = 0, ipi_at_a1, ipi_at_a2, ipi_at_b1, ipi_at_b2 };
