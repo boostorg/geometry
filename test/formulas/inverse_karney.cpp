a826d99c6ca43473a9e8d0273149e83235003310
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2019-11-06 00:17:39 +0100

    [test][formulas] Extract karney_inverse tests into separate file.

--- test/formulas/inverse_karney.cpp
+++ test/formulas/inverse_karney.cpp
@@ -18,10 +18,10 @@
 
 #include "test_formula.hpp"
 #include "inverse_cases.hpp"
+#include "inverse_cases_antipodal.hpp"
+#include "inverse_cases_small_angles.hpp"
 
-#include <boost/geometry/formulas/vincenty_inverse.hpp>
-#include <boost/geometry/formulas/thomas_inverse.hpp>
-#include <boost/geometry/formulas/andoyer_inverse.hpp>
+#include <boost/geometry/formulas/karney_inverse.hpp>
 
 #include <boost/geometry/srs/spheroid.hpp>
 
@@ -50,36 +50,37 @@ void check_inverse(std::string const& name,
 
 void test_all(expected_results const& results)
 {
-    double const d2r = bg::math::d2r<double>();
-    double const r2d = bg::math::r2d<double>();
-
-    double lon1r = results.p1.lon * d2r;
-    double lat1r = results.p1.lat * d2r;
-    double lon2r = results.p2.lon * d2r;
-    double lat2r = results.p2.lat * d2r;
+    double lon1d = results.p1.lon;
+    double lat1d = results.p1.lat;
+    double lon2d = results.p2.lon;
+    double lat2d = results.p2.lat;
 
     // WGS84
     bg::srs::spheroid<double> spheroid(6378137.0, 6356752.3142451793);
 
-    bg::formula::result_inverse<double> result_v, result_t, result_a;
+    bg::formula::result_inverse<double> result_k;
 
-    typedef bg::formula::vincenty_inverse<double, true, true, true, true, true> vi_t;
-    result_v = vi_t::apply(lon1r, lat1r, lon2r, lat2r, spheroid);
-    result_v.azimuth *= r2d;
-    result_v.reverse_azimuth *= r2d;
-    check_inverse("vincenty", results, result_v, results.vincenty, results.reference, 0.0000001);
+    typedef bg::formula::karney_inverse<double, true, true, true, true, true, 8> ka_t;
+    result_k = ka_t::apply(lon1d, lat1d, lon2d, lat2d, spheroid);
+    check_inverse("karney", results, result_k, results.vincenty, results.reference, 0.0000001);
+}
 
-    typedef bg::formula::thomas_inverse<double, true, true, true, true, true> th_t;
-    result_t = th_t::apply(lon1r, lat1r, lon2r, lat2r, spheroid);
-    result_t.azimuth *= r2d;
-    result_t.reverse_azimuth *= r2d;
-    check_inverse("thomas", results, result_t, results.thomas, results.reference, 0.00001);
+template <typename ExpectedResults>
+void test_karney(ExpectedResults const& results)
+{
+    double lon1d = results.p1.lon;
+    double lat1d = results.p1.lat;
+    double lon2d = results.p2.lon;
+    double lat2d = results.p2.lat;
 
-    typedef bg::formula::andoyer_inverse<double, true, true, true, true, true> an_t;
-    result_a = an_t::apply(lon1r, lat1r, lon2r, lat2r, spheroid);
-    result_a.azimuth *= r2d;
-    result_a.reverse_azimuth *= r2d;
-    check_inverse("andoyer", results, result_a, results.andoyer, results.reference, 0.001);
+    // WGS84
+    bg::srs::spheroid<double> spheroid(6378137.0, 6356752.3142451793);
+
+    bg::formula::result_inverse<double> result;
+
+    typedef bg::formula::karney_inverse<double, true, true, true, true, true, 8> ka_t;
+    result = ka_t::apply(lon1d, lat1d, lon2d, lat2d, spheroid);
+    check_inverse("karney", results, result, results.karney, results.karney, 0.0000001);
 }
 
 int test_main(int, char*[])
@@ -87,6 +88,16 @@ int test_main(int, char*[])
     for (size_t i = 0; i < expected_size; ++i)
     {
         test_all(expected[i]);
+    }
+
+    for (size_t i = 0; i < expected_size_antipodal; ++i)
+    {
+        test_karney(expected_antipodal[i]);
+    }
+
+    for (size_t i = 0; i < expected_size_small_angles; ++i)
+    {
+        test_karney(expected_small_angles[i]);
     }
 
     return 0;
