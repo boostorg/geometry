boostorg/geometry

commit 60405cef79792ebe22342e2473735b35718fde61

[doc] Added missing algorithm examples in documentation. (#667)
* added perimeter documentation

* added overlaps documentation

* added covered_by documentation

* added disjoint documentation

* added crosses documentation

* added touches documentation

* fixed typos

* added examples with images to documentation

* completed linking process

* fixed year

doc/src/examples/algorithms/touches_one_geometry.cpp
@@ -1,0 +1,59 @@
+// Boost.Geometry (aka GGL, Generic Geometry Library)
+// QuickBook Example
+
+// Copyright (c) 2020 Digvijay Janartha, Hamirpur, India.
+
+// Use, modification and distribution is subject to the Boost Software License,
+// Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+//[touches
+//` Checks if a geometry has at least one touching point (self-tangency)
+
+#include <iostream>
+
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/point_xy.hpp>
+#include <boost/geometry/geometries/polygon.hpp>
+
+namespace bg = boost::geometry; /*< Convenient namespace alias >*/
+
+int main()
+{
+    // Checks if the geometry has self-tangency.
+    bg::model::polygon<bg::model::d2::point_xy<double> > poly1;
+    bg::read_wkt("POLYGON((0 0,0 3,2 3,2 2,1 2,1 1,2 1,2 2,3 2,3 0,0 0))", poly1);
+    bool check_touches = bg::touches(poly1);
+    if (check_touches) {
+         std::cout << "Touches: Yes" << std::endl;
+    } else {
+        std::cout << "Touches: No" << std::endl;
+    }
+
+    bg::model::polygon<bg::model::d2::point_xy<double> > poly2;
+    bg::read_wkt("POLYGON((0 0,0 4,4 4,4 0,2 3,0 0))", poly2);
+    check_touches = bg::touches(poly2);
+    if (check_touches) {
+         std::cout << "Touches: Yes" << std::endl;
+    } else {
+        std::cout << "Touches: No" << std::endl;
+    }
+
+    return 0;
+}
+
+//]
+
+
+//[touches_output
+/*`
+Output:
+[pre
+Touches: Yes
+
+[$img/algorithms/touches_one_geometry.png]
+
+Touches: No
+]
+*/
+//]

doc/src/examples/algorithms/touches_two_geometries.cpp
@@ -1,0 +1,61 @@
+// Boost.Geometry (aka GGL, Generic Geometry Library)
+// QuickBook Example
+
+// Copyright (c) 2020 Digvijay Janartha, Hamirpur, India.
+
+// Use, modification and distribution is subject to the Boost Software License,
+// Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+//[touches
+//` Checks if two geometries have at least one touching point (tangent - non overlapping)
+
+#include <iostream>
+
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/point_xy.hpp>
+#include <boost/geometry/geometries/polygon.hpp>
+
+namespace bg = boost::geometry; /*< Convenient namespace alias >*/
+
+int main()
+{
+    // Checks if the two geometries overlaps or not. 
+    bg::model::polygon<bg::model::d2::point_xy<double> > poly1;
+    bg::read_wkt("POLYGON((0 0,0 4,4 4,4 0,0 0))", poly1);
+    bg::model::polygon<bg::model::d2::point_xy<double> > poly2;
+    bg::read_wkt("POLYGON((0 0,0 -4,-4 -4,-4 0,0 0))", poly2);
+    bool check_touches = bg::touches(poly1, poly2);
+    if (check_touches) {
+         std::cout << "Touches: Yes" << std::endl;
+    } else {
+        std::cout << "Touches: No" << std::endl;
+    }
+
+    bg::model::polygon<bg::model::d2::point_xy<double> > poly3;
+    bg::read_wkt("POLYGON((1 1,0 -4,-4 -4,-4 0,1 1))", poly3);
+    check_touches = bg::touches(poly1, poly3);
+    if (check_touches) {
+         std::cout << "Touches: Yes" << std::endl;
+    } else {
+        std::cout << "Touches: No" << std::endl;
+    }
+
+    return 0;
+}
+
+//]
+
+
+//[touches_output
+/*`
+Output:
+[pre
+Touches: Yes
+
+[$img/algorithms/touches_two_geometries.png]
+
+Touches: No
+]
+*/
+//]

commit 53a6ada73c09b3250be66419e797fa2c81134687

Fix quickbook template redefinition in algorithm examples
Correct example comment (copy/paste error).

Fixes issue introduced in #667

doc/src/examples/algorithms/touches_one_geometry.cpp
@@ -7,7 +7,7 @@
 // Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
-//[touches
+//[touches_one_geometry
 //` Checks if a geometry has at least one touching point (self-tangency)
 
 #include <iostream>
@@ -45,7 +45,7 @@ int main()
 //]
 
 
-//[touches_output
+//[touches_one_geometry_output
 /*`
 Output:
 [pre

doc/src/examples/algorithms/touches_two_geometries.cpp
@@ -7,7 +7,7 @@
 // Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
-//[touches
+//[touches_two_geometries
 //` Checks if two geometries have at least one touching point (tangent - non overlapping)
 
 #include <iostream>
@@ -20,7 +20,7 @@ namespace bg = boost::geometry; /*< Convenient namespace alias >*/
 
 int main()
 {
-    // Checks if the two geometries overlaps or not. 
+    // Checks if the two geometries touch
     bg::model::polygon<bg::model::d2::point_xy<double> > poly1;
     bg::read_wkt("POLYGON((0 0,0 4,4 4,4 0,0 0))", poly1);
     bg::model::polygon<bg::model::d2::point_xy<double> > poly2;
@@ -47,7 +47,7 @@ int main()
 //]
 
 
-//[touches_output
+//[touches_two_geometries_output
 /*`
 Output:
 [pre