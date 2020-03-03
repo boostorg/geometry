d9c8ff3cc281feed32d14dd2f8040cf773cfaf12
sudo-panda <kundubaidya99@gmail.com>
2020-03-03 11:08:26 +0530

    [doc][io][wkt] fix bugs in index.html and example file - Restored index.html back - Declared variables first and then used in example for from_wkt

--- doc/src/examples/io/from_wkt.cpp
+++ doc/src/examples/io/from_wkt.cpp
@@ -18,16 +18,21 @@
 int main()
 {
     typedef boost::geometry::model::d2::point_xy<double> point_type;
-    typedef boost::geometry::model::linestring<point_type> linestring_type;
-    typedef boost::geometry::model::polygon<point_type> polygon_type;
     typedef boost::geometry::model::box<point_type> box_type;
     typedef boost::geometry::model::segment<point_type> segment_type;
+    typedef boost::geometry::model::polygon<point_type> polygon_type;
+    typedef boost::geometry::model::linestring<point_type> linestring_type;
 
-    point_type a = boost::geometry::from_wkt<point_type>("POINT(1 2)");
-    linestring_type b = boost::geometry::from_wkt<linestring_type>("LINESTRING(0 0,2 2,3 1)");
-    polygon_type c = boost::geometry::from_wkt<polygon_type>("POLYGON((0 0,0 7,4 2,2 0,0 0))");
-    box_type d = boost::geometry::from_wkt<box_type>("BOX(0 0,3 3)");
-    segment_type e = boost::geometry::from_wkt<segment_type>("SEGMENT(1 0,3 4)");
+    point_type a;
+    box_type d;
+    segment_type e;
+    polygon_type c;
+    linestring_type b;
+    a = boost::geometry::from_wkt<point_type>("POINT(1 2)");
+    d = boost::geometry::from_wkt<box_type>("BOX(0 0,3 3)");
+    e = boost::geometry::from_wkt<segment_type>("SEGMENT(1 0,3 4)");
+    c = boost::geometry::from_wkt<polygon_type>("POLYGON((0 0,0 7,4 2,2 0,0 0))");
+    b = boost::geometry::from_wkt<linestring_type>("LINESTRING(0 0,2 2,3 1)");
 
     return 0;
 }
