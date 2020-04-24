a7ce2af149382e468c2f2d23a9df0c561119917d
sudo-panda <kundubaidya99@gmail.com>
2020-04-24 22:53:10 +0530

    [doc][ext][io] Change ex to check prev commit

--- extensions/example/io/plotly_example.cpp
+++ extensions/example/io/plotly_example.cpp
@@ -11,21 +11,32 @@
 #include <fstream>
 #include <boost/geometry.hpp>
 #include <boost/geometry/extensions/io/plotly/plotter.hpp>
-#include <boost/geometry/geometries/point_xyz.hpp>
+#include <boost/geometry/geometries/point_xy.hpp>
+
+namespace bg = boost::geometry;
 
 int main()
 {
-    typedef boost::geometry::model::d3::point_xyz<double> point_t;
+    typedef bg::model::point<double, 2, bg::cs::cartesian> point_t;
+    typedef bg::model::polygon<point_t> polygon_t;
+
+    polygon_t poly1;
+
+    bg::append(poly1.outer(), point_t(0.0, 0.0));
+    bg::append(poly1.outer(), point_t(0.0, 5.0));
+    bg::append(poly1.outer(), point_t(5.0, 5.0));
+    bg::append(poly1.outer(), point_t(5.0, 0.0));
+    bg::append(poly1.outer(), point_t(0.0, 0.0));
+
+    poly1.inners().resize(1);
+    bg::append(poly1.inners()[0], point_t(1.0, 1.0));
+    bg::append(poly1.inners()[0], point_t(4.0, 1.0));
+    bg::append(poly1.inners()[0], point_t(4.0, 4.0));
+    bg::append(poly1.inners()[0], point_t(1.0, 4.0));
+    bg::append(poly1.inners()[0], point_t(1.0, 1.0));
     
-    boost::geometry::model::linestring<point_t> a;
-
-    boost::geometry::append(a, point_t(0, 0, 0));
-    boost::geometry::append(a, point_t(0, 1, 2));
-    boost::geometry::append(a, point_t(1, 2, 3));
-
-    std::ofstream json("my_plot.json");
-    boost::geometry::json_plotter plotter(json);
-    plotter.plot(a, "rgb(5, 200, 64)");
+    bg::plotly_plotter<std::ofstream> plotter("my_plot.json");
+    plotter.plot(poly1, bg::plotly_color(255, 39, 0));
 
     return 0;
 }
