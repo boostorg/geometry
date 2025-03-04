fb99e6f3e87f7e1332ae8274dcc5065612d193e8
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2025-03-04 23:05:14 +0100

    test: divide rtree_with_strategies test into multiple files

---
+++ index/test/rtree/strategies/rtree_with_strategies_s_r.cpp
@@ -0,0 +1,17 @@
+// Boost.Geometry Index
+// Unit Test
+
+// Copyright (c) 2025 Adam Wulkiewicz, Lodz, Poland.
+
+// Use, modification and distribution is subject to the Boost Software License,
+// Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#include "rtree_with_strategies.hpp"
+
+int test_main(int, char* [])
+{
+    test_rtree<segment, bgi::rstar<4>>();
+
+    return 0;
+}
